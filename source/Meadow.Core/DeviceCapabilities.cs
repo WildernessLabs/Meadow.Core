b1a1a8136b5b026e0a0b1ebf87a12509606ea83c
bryan costanich <b@wowzer.net>
2018-11-26 17:04:28 -0800

    moved some foundation stuff into core

--- source/Meadow.Core/DeviceCapabilities.cs
+++ source/Meadow.Core/DeviceCapabilities.cs
@@ -3,10 +3,22 @@ namespace Meadow
 {
     public class DeviceCapabilities
     {
+        public NetworkCapabilities Network
+        {
+            get; protected set;
+        }
+
         public DeviceCapabilities()
         {
         }
 
 
     }
+
+    public class NetworkCapabilities
+    {
+        public bool HasWiFi { get; protected set; }
+        public bool HasEthernet { get; protected set; }
+    }
 }
+
---
+++ source/Meadow.Core/Enumerations/ByteOrder.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Meadow
+{
+    /// <summary>
+    /// Indicate the byte ordering for muli-byte words.
+    /// </summary>
+    public enum ByteOrder { LittleEndian, BigEndian };
+}
