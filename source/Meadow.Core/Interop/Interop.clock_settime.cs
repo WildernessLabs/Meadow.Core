01b76ec95257afba292c9beef5acec76bddac217
Chris Tacke <ctacke@opennetcf.com>
2018-12-08 12:01:24 -0600

    Added Interop Added clock P/Invokes Changed to .NET 4.7.1

---
+++ source/Meadow.Core/Interop/Interop.clock_settime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Meadow.Core.Interop
+{
+    internal static partial class Interop
+    {
+        public static partial class Nuttx
+        {
+            [DllImport("nuttx", SetLastError = true)]
+            public static extern int clock_settime(clockid_t clk_id, ref timespec tp);
+        }
+    }
+}
---
+++ source/Tests/ClockPInvoke.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Meadow.Core.Interop;
+
+namespace ClockPInvoke.Test
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program Started (console)!");
+
+            while (true)
+            {
+                Console.WriteLine("Before P/Invoke");
+
+                Interop.Nuttx.timespec ts = new Interop.Nuttx.timespec();
+                var time = Interop.Nuttx.clock_gettime(Interop.Nuttx.clockid_t.CLOCK_REALTIME, ref ts);
+
+                Console.WriteLine("Result: " + ts.tv_sec.ToString());
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
