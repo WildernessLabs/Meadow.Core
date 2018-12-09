a22b8315b8f3e3e38afe8bb45a988212bbb2eff6
bryan costanich <b@wowzer.net>
2018-12-08 16:41:20 -0800

    added a GPIO stub call

--- source/Meadow.Core/OSInterop/Platform.cs
+++ source/Meadow.Core/OSInterop/Platform.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+
 namespace Meadow.OSInterop
 {
     internal partial class Platform
     {
-        //internal partial class Nuttx
-        //{
-        //    [DllImport(ConstValueForLibraryName, SetLastError = true)]
-        //    internal static extern void Foo();
-        //}
+        private const string NUTTX = "NuttXOrWhatevers";
+
+        internal partial class Nuttx
+        {
+            //    [DllImport(ConstValueForLibraryName, SetLastError = true)]
+            //    internal static extern void Foo();
+
+            //stm32_configgpio
+            [DllImport(NUTTX, SetLastError = true)]
+            internal static extern void Stm32_configgpio(uint configSet);
+
+        }
+
+
     }
 }
