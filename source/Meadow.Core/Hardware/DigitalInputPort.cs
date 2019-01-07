b02faae42c31500dcefa3563bfebfe6bccedb788
Chris Tacke <ctacke@opennetcf.com>
2019-01-07 10:22:51 -0600

    implemented digital inputs

--- source/Meadow.Core/Hardware/DigitalInputPort.cs
+++ source/Meadow.Core/Hardware/DigitalInputPort.cs
@@ -4,19 +4,33 @@ namespace Meadow.Hardware
 {
     public class DigitalInputPort : DigitalInputPortBase
     {
+        protected IDigitalPin _pin;
+        protected bool _disposed;
+
         public bool GlitchFilter { get; set; }
         public ResistorMode Resistor { get; set; }
 
-        public DigitalInputPort(IDigitalChannel pin, bool glitchFilter, ResistorMode resistorMode)
+        public DigitalInputPort(IDigitalPin pin, bool glitchFilter = false, ResistorMode resistorMode = ResistorMode.Disabled)
         {
+            // attempt to reserve the pin
+            var result = DeviceChannelManager.ReservePin(pin, ChannelConfigurationType.DigitalInput);
 
+            if(result.Item1)
+            {
+                this._pin = pin;
+
+                // make sure the pin is configured as a digital output
+                _pin.GPIOManager.Configure(_pin, glitchFilter, resistorMode);
+            }
+            else
+            {
+                throw new PortInUseException();
+            }
         }
 
-        public DigitalInputPort()
+        public override bool Value
         {
-
+            get => _pin.GPIOManager.GetDiscrete(_pin);
         }
-
-        public override bool Value => throw new NotImplementedException();
     }
 }
