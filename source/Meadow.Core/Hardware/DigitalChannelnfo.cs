47ce7c09d463f28c09ee54feb2c1c7c0503cb415
bryan costanich <b@wowzer.net>
2019-01-23 13:58:09 -0800

    looks like this got missed somehow.

--- source/Meadow.Core/Hardware/DigitalChannelnfo.cs
+++ source/Meadow.Core/Hardware/DigitalChannelnfo.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 namespace Meadow.Hardware
 {
-    public class DigitalChannelnfo
+    public class DigitalChannelnfo : IDigitalChannelInfo
     {
         public DigitalChannelnfo()
         {
         }
+
+        public bool InterrruptCapable { get; set; } = false;
+        public bool PullDownCapable { get; set; } = false;
+        public bool PullUpCapable { get; set; } = false;
     }
 }
