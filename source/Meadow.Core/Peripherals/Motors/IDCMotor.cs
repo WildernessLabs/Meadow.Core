f1ae76d7b9430a387ad1f680c7e3aa8c4264a354
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-26 00:10:09 -0800

    Add IDCMotor

---
+++ source/Meadow.Core/Peripherals/Motors/IDCMotor.cs
@@ -0,0 +1,18 @@
+﻿namespace Meadow.Peripherals.Motors
+{
+    /// <summary>
+    /// Interface describing DC motors.
+    /// </summary>
+    public interface IDCMotor
+    {
+        /// <summary>
+        /// The speed of the motor from -1 to 1.
+        /// </summary>
+        float Speed { get; set; }
+
+        /// <summary>
+        /// When true, the wheels spin "freely"
+        /// </summary>
+        bool IsNeutral { get; set; }
+    }
+}
