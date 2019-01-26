260131f16d0d0832c36b117ef589c570546d766f
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-25 22:00:07 -0800

    Add IPidController

---
+++ source/Meadow.Core/Peripherals/Controllers/PID/IPidController.cs
@@ -0,0 +1,59 @@
+﻿namespace Meadow.Peripherals.Controllers.PID
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPidController
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        float OutputMin { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float OutputMax { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float ActualInput { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float TargetInput { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float IntegralComponent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float DerivativeComponent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        float ProportionalComponent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        bool OutputTuningInformation { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        void ResetIntegrator();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        float CalculateControlOutput();
+    }
+}
