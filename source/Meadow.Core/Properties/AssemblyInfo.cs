4abfba53b4db01f6afed1bb8705800066b67a5ca
Brian Kim <lamebrain@gmail.com>
2019-01-25 23:03:30 -0800

    bump and push

--- source/Meadow.Core/Properties/AssemblyInfo.cs
+++ source/Meadow.Core/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("Meadow.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Meadow.Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("Wilderness Labs, Inc")]
+[assembly: AssemblyProduct("Meadow")]
 [assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
