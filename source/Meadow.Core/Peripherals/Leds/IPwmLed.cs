35eec3482cc8c9a2991435f52d9664bac68ae46c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-25 21:13:02 -0800

    Add IPwmLed

---
+++ source/Meadow.Core/Peripherals/Leds/IPwmLed.cs
@@ -0,0 +1,13 @@
+﻿using Meadow.Hardware;
+
+namespace Meadow.Peripherals.Leds
+{
+    /// <summary>
+    /// Defines a Light Emitting Diode (LED) controlled by a pulse-width-modulation
+    /// (PWM) signal to limit current.
+    /// </summary>
+    public interface IPwmLed : ILed
+    {
+        new IPwmPort Port { get; }
+    }
+}
