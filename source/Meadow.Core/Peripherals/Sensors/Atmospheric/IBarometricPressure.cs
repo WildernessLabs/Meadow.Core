98d3eb0cc74f58dbb2f519222385366e213ff92f
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-27 00:34:44 -0800

    Add IHumiditySensor and IBaromatricPressure (FKA IPressure)

---
+++ source/Meadow.Core/Peripherals/Sensors/Atmospheric/IBarometricPressure.cs
@@ -0,0 +1,25 @@
+﻿namespace Meadow.Peripherals.Sensors.Atmospheric
+{
+    /// <summary>
+    /// Pressure sensor interface requirements.
+    /// </summary>
+    public interface IBarometricPressure
+    {
+        /// <summary>
+        /// Last value read from the Pressure sensor.
+        /// </summary>
+        float Pressure { get; }
+
+        /// <summary>
+        /// Threshold value for the changed notification event.
+        /// </summary>
+        float PressureChangeNotificationThreshold { get; set; }
+
+        /// <summary>
+        /// The PressureChanged event will be raised when the difference (absolute value)
+        /// between the current Pressure reading and the last notified reading is greater
+        /// than the PressureChangeNotificationThreshold.
+        /// </summary>
+        event SensorFloatEventHandler PressureChanged;
+    }
+}
