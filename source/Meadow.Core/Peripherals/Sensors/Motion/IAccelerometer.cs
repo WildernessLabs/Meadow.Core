e0347de294946eb5e2b59c6a9c7623404448e27b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-26 19:19:08 -0800

    Add IAccelerometer

---
+++ source/Meadow.Core/Peripherals/Sensors/Motion/IAccelerometer.cs
@@ -0,0 +1,23 @@
+﻿namespace Meadow.Peripherals.Sensors.Motion
+{
+    /// <summary>
+    /// Represents a generic accelerometer sensor.
+    /// </summary>
+    public interface IAccelerometer
+    {
+        /// <summary>
+        /// Returns acceleration value in the X axis
+        /// </summary>
+        float XAcceleration { get; }
+
+        /// <summary>
+        /// Returns acceleration value in the Y axis
+        /// </summary>
+        float YAcceleration { get; }
+
+        /// <summary>
+        /// Returns acceleration value in the Z axis
+        /// </summary>
+        float ZAcceleration { get; }
+    }
+}
