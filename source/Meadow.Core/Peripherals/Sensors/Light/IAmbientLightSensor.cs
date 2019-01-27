69a0245cd97060948be283733b12fd8445076f83
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-26 19:01:36 -0800

    Add IambientLightSensor

---
+++ source/Meadow.Core/Peripherals/Sensors/Light/IAmbientLightSensor.cs
@@ -0,0 +1,13 @@
+﻿namespace Meadow.Peripherals.Sensors.Light
+{
+    /// <summary>
+    /// Generic ambient light sensor class
+    /// </summary>
+    public interface IAmbientLightSensor
+    {
+        /// <summary>
+        /// Returns raw data from the sensor
+        /// </summary>
+        float Reading { get; }
+    }
+}
