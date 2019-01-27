93c42e532e8a261152714730bef2b53aca8c9b30
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-26 23:28:25 -0800

    Add ISwitch

---
+++ source/Meadow.Core/Peripherals/Switches/ISwitch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Meadow.Peripherals.Switches
+{
+    /// <summary>
+    /// Represents a generic switch
+    /// </summary>
+    public interface ISwitch
+    {
+        /// <summary>
+        /// Describes whether or not the switch circuit is closed/connected (IsOn = true), or open (IsOn = false).
+        /// </summary>
+        bool IsOn { get; }
+
+        /// <summary>
+        /// Raised when the switch circuit circuit is opened or closed.
+        /// </summary>
+        event EventHandler Changed;
+    }
+}
