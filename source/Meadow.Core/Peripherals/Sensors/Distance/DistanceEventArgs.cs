93636ac00aea197618a30e51959e4e34373c0edb
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-27 00:50:21 -0800

    Add IRangeFilter (Distance) and DistanceEventArgs

---
+++ source/Meadow.Core/Peripherals/Sensors/Distance/DistanceEventArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace Meadow.Peripherals.Sensors.Distance
+{
+    /// <summary>
+    /// Distance event arguments for distance sensors
+    /// </summary>
+    public class DistanceEventArgs
+    {
+        /// <summary>
+        /// Gets or sets distance
+        /// </summary>
+        public float Distance { get; set; }
+
+        /// <summary>
+        /// Creates a distance event argument
+        /// </summary>
+        /// <param name="distance"></param>
+        public DistanceEventArgs(float distance)
+        {
+            Distance = distance;
+        }
+    }
+
+    /// <summary>
+    /// Raised when an received a rebound trigger signal
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void DistanceDetectedEventHandler(object sender, DistanceEventArgs e);
+}
---
+++ source/Meadow.Core/Peripherals/Sensors/Distance/IRangeFinder.cs
@@ -0,0 +1,28 @@
+﻿namespace Meadow.Peripherals.Sensors.Distance
+{
+    /// <summary>
+    /// Interface for distance sensors classes.
+    /// </summary>
+    public interface IRangeFinder
+    {
+        /// <summary>
+        /// Returns current distance detected.
+        /// </summary>
+        float CurrentDistance { get; }
+
+        /// <summary>
+        /// Minimum valid distance (CurrentDistance returns -1 if below).
+        /// </summary>
+        float MinimumDistance { get; }
+
+        /// <summary>
+        /// Maximum valid distance (CurrentDistance returns -1 if above).
+        /// </summary>
+        float MaximumDistance { get; }
+
+        /// <summary>
+        /// Raised when detecting an obstacle.
+        /// </summary>
+        event DistanceDetectedEventHandler DistanceDetected;
+    }
+}
