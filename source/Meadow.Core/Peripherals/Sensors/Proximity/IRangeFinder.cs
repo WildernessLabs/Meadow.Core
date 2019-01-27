17da6560b3ccd230ec1dd94a057f4e2d60eb3f7a
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-01-26 19:34:22 -0800

    Add Proximity IRangeFinder

---
+++ source/Meadow.Core/Peripherals/Sensors/Proximity/IRangeFinder.cs
@@ -0,0 +1,13 @@
+﻿namespace Meadow.Peripherals.Sensors.Proximity
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IRangeFinder
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        float DistanceOutput { get; }
+    }
+}
