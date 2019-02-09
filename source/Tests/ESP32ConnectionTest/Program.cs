5492aac1b58dc53e865f5c4db8e5e473c83a0592
Mark Stevens <mark@thepcsite.co.uk>
2019-02-09 11:13:06 +0000

    ESP32Test: Minor change for UART Rx Test

--- source/Tests/ESP32ConnectionTest/Program.cs
+++ source/Tests/ESP32ConnectionTest/Program.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Threading;
 using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
 
 namespace ESP32ConnectionTest
 {
     class MainClass
     {
-//        static IApp application;
+        static ESP32ConnectionApp application = new ESP32ConnectionApp();
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Testing the ESP32 GPIO Pins");
+            Console.WriteLine("Testing the ESP32 MISO pin in output mode");
 
-//            application = new ESP32ConnectionApp();
-//            application.Run();
+            application.Run();
         }
     }
 }
