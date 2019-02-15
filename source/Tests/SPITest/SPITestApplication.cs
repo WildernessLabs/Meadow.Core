d2091a957f7d14dd2dc3bedb728643ce78d72c7c
Mark Stevens <mark@thepcsite.co.uk>
2019-02-15 11:17:18 +0000

    Tests: Additional test code added.

--- source/Tests/SPITest/SPITestApplication.cs
+++ source/Tests/SPITest/SPITestApplication.cs
@@ -30,20 +30,34 @@ namespace SPITest
             _boot.State = true;
             _reset.State = false;
             _reset.State = true;
+            //SoftwareSPIBus esp32 = new SoftwareSPIBus(Device.Pins.D05, Device.Pins.D06, Device.Pins.D07, Device.Pins.D08);
             SoftwareSPIBus esp32 = new SoftwareSPIBus(Device.Pins.ESP_MOSI, Device.Pins.ESP_MISO, Device.Pins.ESP_CLK, Device.Pins.ESP_CS);
             int counter = 0;
             while (true)
             {
                 _blueLED.State = !_blueLED.State;
-                string message = $".NET Message number {counter}          ";
+                string message = $".NET Message number {counter}";
                 counter++;
                 Console.WriteLine($"Sending message: '{message}'");
                 byte[] buffer = Encoding.UTF8.GetBytes(message);
-                byte[] writeBuffer = new byte[buffer.Length + 1];
+                int esp32BufferLength = buffer.Length + 1;
+                //
+                //  The buffer should be a multiple of four bytes long.
+                //
+                if ((esp32BufferLength & 3) != 0)
+                {
+                    esp32BufferLength &= 0xfffc;
+                    esp32BufferLength += 4;
+                }
+                byte[] writeBuffer = new byte[esp32BufferLength];
+                Array.Clear(writeBuffer, 0, esp32BufferLength);
                 Array.Copy(buffer, writeBuffer, buffer.Length);
-                writeBuffer[buffer.Length] = 0;
+                Console.WriteLine(DebugHelpers.PrintableBuffer(writeBuffer));
                 byte[] dataFromESP32 = esp32.WriteRead(writeBuffer, (ushort) writeBuffer.Length);
-                Console.WriteLine("Message from ESP32: " + Encoding.UTF8.GetString(dataFromESP32));
+                //
+                Console.WriteLine("Message from ESP32:");
+                Console.WriteLine(Encoding.UTF8.GetString(dataFromESP32));
+                Console.WriteLine(DebugHelpers.PrintableBuffer(dataFromESP32));
                 Thread.Sleep(500);
             }
         }
