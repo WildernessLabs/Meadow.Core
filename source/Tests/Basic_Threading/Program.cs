95a9bcb4c433614bff4802e56b4f12ebe1e2fd49
bryan costanich <b@wowzer.net>
2019-03-19 11:31:44 -0700

    Added a basic threading test app.

---
+++ source/Tests/Basic_Threading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Meadow;
+
+namespace Basic_Threading
+{
+    class MainClass
+    {
+        static IApp app;
+
+        public static void Main(string[] args)
+        {
+            app = new ThreadingApp();
+        }
+    }
+}
