06347e2af4be01ceefae977b9b412a27617a984d
bryan costanich <b@wowzer.net>
2019-03-21 18:23:17 -0700

    fixups on HelloLED, and WIP of analog read app

---
+++ source/Tests/Basic_AnalogReads/AnalogReadApp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Meadow;
+using Meadow.Hardware;
+using Meadow.Devices;
+
+namespace Basic_AnalogReads
+{
+    public class AnalogReadApp : AppBase<F7Micro, AnalogReadApp>
+    {
+        public AnalogReadApp()
+        {
+        }
+    }
+}
---
+++ source/Tests/Basic_AnalogReads/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Meadow;
+
+namespace Basic_AnalogReads
+{
+    class MainClass
+    {
+        static IApp app;
+
+        public static void Main(string[] args)
+        {
+            app = new AnalogReadApp();
+        }
+    }
+}
