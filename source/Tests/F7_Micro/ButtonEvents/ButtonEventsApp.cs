7f308c3c7fdff200ccb9810616698f5275c7022e
bryan costanich <b@wowzer.net>
2019-03-25 12:16:09 -0700

    Added debounce to

--- source/Tests/F7_Micro/ButtonEvents/ButtonEventsApp.cs
+++ source/Tests/F7_Micro/ButtonEvents/ButtonEventsApp.cs
@@ -11,13 +11,13 @@ namespace ButtonEvents
 
         public ButtonEventsApp()
         {
-            _input = Device.CreateDigitalInputPort(Device.Pins.D02, InterruptMode.EdgeBoth);
+            _input = Device.CreateDigitalInputPort(Device.Pins.D02, InterruptMode.EdgeBoth, debounceDuration: 20);
             _input.Changed += Input_Changed;
         }
 
         private void Input_Changed(object sender, DigitalInputPortEventArgs e)
         {
-            Console.WriteLine("Changed: " + e.Value.ToString() + " Time: " + e.Time.ToString());
+            Console.WriteLine("Changed: " + e.Value.ToString() + ", Time: " + e.Time.ToString());
         }
     }
 }
