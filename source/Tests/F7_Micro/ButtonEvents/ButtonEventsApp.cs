5d516c9e5dbccaf4a521f2d41aff837d54082ae9
bryan costanich <b@wowzer.net>
2019-03-29 14:34:58 -0700

    Added circuit notes

--- source/Tests/F7_Micro/ButtonEvents/ButtonEventsApp.cs
+++ source/Tests/F7_Micro/ButtonEvents/ButtonEventsApp.cs
@@ -5,6 +5,13 @@ using Meadow.Devices;
 
 namespace ButtonEvents
 {
+    /// <summary>
+    /// This sample illustrates simple input events using a PushButton. To wire up, add
+    /// a PushButton connected to D02, with the circuit terminating on the 3.3V rail, so that
+    /// when the button is pressed, the input is raised high. Because internal pull-downs are
+    /// not currently working, add a 10k pull-down resistor to the input side as illustrated in
+    /// http://developer.wildernesslabs.co/Hardware/Tutorials/Electronics/Part4/PullUp_PullDown_Resistors/
+    /// </summary>
     public class ButtonEventsApp : AppBase<F7Micro, ButtonEventsApp>
     {
         IDigitalInputPort _input;
