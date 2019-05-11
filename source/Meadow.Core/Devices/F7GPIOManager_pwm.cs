7cd539d608234aea3936bb7606a84e9353258c01
Chris Tacke <ctacke@opennetcf.com>
2019-05-11 10:29:56 -0500

    More PWM testing - based on nuttx driver

--- source/Meadow.Core/Devices/F7GPIOManager_pwm.cs
+++ source/Meadow.Core/Devices/F7GPIOManager_pwm.cs
@@ -31,14 +31,33 @@ namespace Meadow.Devices
         private const int TIMx_CCR1_OFFSET = 0x34;  // capture/compare register 1
         private const int TIMx_CCR2_OFFSET = 0x38;
         private const int TIMx_CCR3_OFFSET = 0x3c;
+        private const int TIMx_CCR4_OFFSET = 0x40;
         private const int TIMx_BDTR_OFFSET = 0x44;
 
         private const int RCC_AHB1RSTR_OFFSET = 0x10;
         private const int RCC_AHB1ENR_OFFSET = 0x30;
+        private const int RCC_APB1ENR_OFFSET = 0x40;
+        private const int RCC_APB2ENR_OFFSET = 0x44;
 
         private const uint RCC_AHB1ENR_GPIOAEN = 1 << 0;
         private const uint RCC_AHB1ENR_GPIOBEN = 1 << 1;
         private const uint RCC_AHB1ENR_GPIOCEN = 1 << 2;
+
+        private const uint RCC_APB1ENR_TIM2EN = 1 << 0;
+        private const uint RCC_APB1ENR_TIM3EN = 1 << 1;
+        private const uint RCC_APB1ENR_TIM4EN = 1 << 2;
+        private const uint RCC_APB1ENR_TIM5EN = 1 << 3;
+        private const uint RCC_APB1ENR_TIM6EN = 1 << 4;
+        private const uint RCC_APB1ENR_TIM7EN = 1 << 5;
+        private const uint RCC_APB1ENR_TIM12EN = 1 << 6;
+        private const uint RCC_APB1ENR_TIM13EN = 1 << 7;
+        private const uint RCC_APB1ENR_TIM14EN = 1 << 8;
+
+        private const uint RCC_APB2ENR_TIM1EN = 1 << 0;
+        private const uint RCC_APB2ENR_TIM8EN = 1 << 1;
+        private const uint RCC_APB2ENR_ADC1EN = 1 << 8;
+        private const uint RCC_APB2ENR_ADC2EN = 1 << 9;
+        private const uint RCC_APB2ENR_ADC3EN = 1 << 10;
 
         private const int UG_SHIFT = 0;
 
@@ -70,7 +89,7 @@ namespace Meadow.Devices
 
         private const uint TIM_CCMR1_CC1S = 0x03 << 0;
         private const uint TIM_CCMR1_CC2S = 0x03 << 8;
-       
+
         private const uint TIM_CCMR1_OC1FE = 1 << 2;
         private const uint TIM_CCMR1_OC1PE = 1 << 3;
         private const uint TIM_CCMR1_OC1M = 0x1007 << 4;
@@ -83,6 +102,11 @@ namespace Meadow.Devices
         private const uint TIM_CCMR2_OC3M = 0x1007 << 4;
         private const uint TIM_CCMR2_OC3PE = 1 << 3;
         private const uint TIM_CCMR2_OC3FE = 1 << 2;
+
+        private const uint TIM_CCMR2_CC4S = 0x03 << 8;
+        private const uint TIM_CCMR2_OC4M = 0x1007 << 12;
+        private const uint TIM_CCMR2_OC4PE = 1 << 11;
+        private const uint TIM_CCMR2_OC4FE = 1 << 10;
 
         private const uint TIM_CR1_CEN = 1 << 0;
         private const uint TIM_CR1_DIR = 1 << 4;
@@ -105,6 +129,9 @@ namespace Meadow.Devices
         private const uint TIM_CCER_CC3P = 1 << 9;
         private const uint TIM_CCER_CC3NE = 1 << 10;
         private const uint TIM_CCER_CC3NP = 1 << 11;
+        private const uint TIM_CCER_CC4E = 1 << 12;
+        private const uint TIM_CCER_CC4P = 1 << 13;
+        private const uint TIM_CCER_CC4NP = 1 << 15;
 
         private const uint TIM_BDTR_MOE = 1 << 15;
 
@@ -183,7 +210,7 @@ namespace Meadow.Devices
             Nuttx.TryGetRegister(DriverHandle, timerBaseAddress + TIMx_SMCR_OFFSET, out uint smcr);
 
             // If the timer supports ADC synchronization through TRGO2, set the master mode selection 2 
-            if(isTIM1or8) // IS_TIM_TRGO2_INSTANCE // this is for TIM1 and TIM8 - TIM8 *is* used by Meadow
+            if (isTIM1or8) // IS_TIM_TRGO2_INSTANCE // this is for TIM1 and TIM8 - TIM8 *is* used by Meadow
             {
                 // Clear the MMS2 bits
                 cr2 &= ~TIM_CR2_MMS2;
@@ -470,7 +497,7 @@ namespace Meadow.Devices
             }
             Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCER_OFFSET, ccer);
 
-            if(timerBaseAddress == TIM8_BASE_ADDRESS)
+            if (timerBaseAddress == TIM8_BASE_ADDRESS)
             {
                 // Enable the main output
                 Nuttx.TryGetRegister(DriverHandle, timerBaseAddress + TIMx_BDTR_OFFSET, out uint bdtr);
@@ -483,9 +510,22 @@ namespace Meadow.Devices
             Nuttx.SetRegister(DriverHandle, timerBaseAddress + TIMx_CR1_OFFSET, cr1);
         }
 
-        public void EnablePC6PWM()
+        private const uint RCC_CFGR_OFFSET = 0x08;
+
+        private const uint RCC_CFGR_PPRE1 = 0x07 << 10;
+        private const uint RCC_CFGR_PPRE2 = 0x07 << 13;
+
+        public void EnablePC6PWM2()
         {
             Console.WriteLine("+EnablePC6PWM");
+
+            // MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, RCC_HCLK_DIV16);
+            // MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, (RCC_HCLK_DIV16 << 3));
+            // MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, RCC_ClkInitStruct->AHBCLKDivider);
+            // __HAL_RCC_SYSCLK_CONFIG(RCC_ClkInitStruct->SYSCLKSource);
+            // MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, RCC_ClkInitStruct->APB1CLKDivider);
+            // MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, ((RCC_ClkInitStruct->APB2CLKDivider) << 3));
+
 
             // this is purely a hard-coded, straight-line test to generate a PWM on a known channel.  
             // It will get refactored once it's working
@@ -501,9 +541,9 @@ namespace Meadow.Devices
             uint pulse = 3000;
 
             // enable GPIO port C clock
-            Nuttx.TryGetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, out uint rcc);
-            rcc |= RCC_AHB1ENR_GPIOCEN;
-            Nuttx.SetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, rcc);
+            Nuttx.TryGetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, out uint ahb1);
+            ahb1 |= RCC_AHB1ENR_GPIOCEN;
+            Nuttx.SetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, ahb1);
 
             // do a reset on the GPIO
             Nuttx.SetRegister(DriverHandle, STM32.GPIOC_BASE + STM32.STM32_GPIO_BSRR_OFFSET, gpioPin << 16);
@@ -512,6 +552,11 @@ namespace Meadow.Devices
             ConfigureGpio(STM32.GpioPort.PortC, 2, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
             ConfigureGpio(STM32.GpioPort.PortC, 5, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
             ConfigureGpio(STM32.GpioPort.PortC, 6, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
+
+            // enable peripheral clock
+            Nuttx.TryGetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_APB1ENR_OFFSET, out uint apb1);
+            apb1 |= RCC_APB1ENR_TIM3EN;
+            Nuttx.SetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_APB1ENR_OFFSET, apb1);
 
             //// Base initialization
             TIMxInit(TIM3_BASE_ADDRESS, prescaler, period, counterMode, autoReloadPreload);
@@ -531,6 +576,174 @@ namespace Meadow.Devices
             TIM_PWM_Start(TIM3_BASE_ADDRESS, TIM_CHANNEL_1, true);
             TIM_PWM_Start(TIM3_BASE_ADDRESS, TIM_CHANNEL_2, true);
             TIM_PWM_Start(TIM3_BASE_ADDRESS, TIM_CHANNEL_3, true);
+
+            Console.WriteLine("-EnablePC6PWM");
+        }
+
+        private const uint ATIM_CCMR_CCS_CCOUT = 0x00;
+        private const int ATIM_CCMR1_CC1S_SHIFT = 0;
+        private const int ATIM_CCMR1_CC2S_SHIFT = 8;
+        private const int ATIM_CCMR2_CC3S_SHIFT = 0;
+        private const int ATIM_CCMR2_CC4S_SHIFT = 8;
+        private const int ATIM_CCMR1_OC1M_SHIFT = 4;
+        private const int ATIM_CCMR1_OC2M_SHIFT = 12;
+        private const int ATIM_CCMR2_OC3M_SHIFT = 4;
+        private const int ATIM_CCMR2_OC4M_SHIFT = 12;
+
+        public void EnablePC6PWM()
+        {
+            Console.WriteLine("+EnablePC6PWM new");
+
+            // enable GPIO port C clock
+            Nuttx.TryGetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, out uint ahb1);
+            ahb1 |= RCC_AHB1ENR_GPIOCEN;
+            Nuttx.SetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_AHB1ENR_OFFSET, ahb1);
+
+            // do a reset on the GPIO
+            Nuttx.SetRegister(DriverHandle, STM32.GPIOC_BASE + STM32.STM32_GPIO_BSRR_OFFSET, 0xff << 16);
+
+            // set the alternate functions
+            ConfigureGpio(STM32.GpioPort.PortC, 2, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
+            ConfigureGpio(STM32.GpioPort.PortC, 5, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
+            ConfigureGpio(STM32.GpioPort.PortC, 6, STM32.GpioMode.Output, STM32.ResistorMode.Float, STM32.GPIOSpeed.Speed_100MHz, STM32.OutputType.PushPull, false, InterruptMode.None);
+
+            // enable peripheral clock
+            Nuttx.TryGetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_APB1ENR_OFFSET, out uint apb1);
+            apb1 |= RCC_APB1ENR_TIM3EN;
+            Nuttx.SetRegister(DriverHandle, RCC_BASE_ADDRESS + RCC_APB1ENR_OFFSET, apb1);
+
+
+            // DEV NOTE: Ported from nuttx STM32F7 PWM driver code
+            uint prescaler = 7;
+            uint timclk = 6000000;
+            uint reload = 60000;
+
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR1_OFFSET, out uint cr1);
+            // disable the timer
+            cr1 &= ~TIM_CR1_CEN;
+
+            cr1 &= ~(TIM_CR1_DIR | TIM_CR1_CMS);
+            cr1 |= TIM_COUNTERMODE_UP;
+            cr1 &= ~TIM_CR1_CKD;
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR1_OFFSET, cr1);
+
+
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_ARR_OFFSET, reload);
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_PSC_OFFSET, prescaler - 1);
+
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_EGR_OFFSET, TIM_EGR_UG);
+
+            var channelCount = 4;
+            uint ccr;
+            uint chanmode;
+            uint ccenable = 0;
+            uint ocmode1 = 0;
+            uint ocmode2 = 0;
+
+            for (var i = 0; i < channelCount; i++)
+            {
+                // duty cycle = ccr / reload (fractional value)
+                ccr = 3000;  // b16toi(duty * reload + b16HALF);
+                chanmode = PWM_MODE_1;
+
+                var channel = i + 1; // channel is 1-based
+                switch (channel)
+                {
+                    case 1:
+                        // Select the CCER enable bit for this channel
+                        ccenable |= TIM_CCER_CC1E;
+
+                        // Set the CCMR1 mode values (leave CCMR2 zero)
+                        ocmode1 |= (ATIM_CCMR_CCS_CCOUT << ATIM_CCMR1_CC1S_SHIFT) |
+                                    (chanmode << ATIM_CCMR1_OC1M_SHIFT) |
+                                    TIM_CCMR1_OC1PE;
+
+                        // Set the duty cycle by writing to the CCR register for this channel
+                        Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCR1_OFFSET, ccr);
+                        break;                    
+                    case 2:
+                        // Select the CCER enable bit for this channel 
+                        ccenable |= TIM_CCER_CC2E;
+
+                        // Set the CCMR1 mode values (leave CCMR2 zero)
+                        ocmode1 |= (ATIM_CCMR_CCS_CCOUT << ATIM_CCMR1_CC2S_SHIFT) |
+                                    (chanmode << ATIM_CCMR1_OC2M_SHIFT) |
+                                    TIM_CCMR1_OC2PE;
+
+                        // Set the duty cycle by writing to the CCR register for this channel
+                        Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCR2_OFFSET, ccr);
+                        break;
+                    case 3:
+                        // Select the CCER enable bit for this channel
+                        ccenable |= TIM_CCER_CC3E;
+
+                        // Set the CCMR2 mode values (leave CCMR1 zero)
+                        ocmode2 |= (ATIM_CCMR_CCS_CCOUT << ATIM_CCMR2_CC3S_SHIFT) |
+                                    (chanmode << ATIM_CCMR2_OC3M_SHIFT) |
+                                    TIM_CCMR2_OC3PE;
+
+                        // Set the duty cycle by writing to the CCR register for this channel
+                        Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCR3_OFFSET, ccr);
+                        break;
+                    case 4:
+                        // Select the CCER enable bit for this channel
+                        ccenable |= TIM_CCER_CC4E;
+
+                        // Set the CCMR2 mode values (leave CCMR1 zero)
+                        ocmode2 |= (ATIM_CCMR_CCS_CCOUT << ATIM_CCMR2_CC4S_SHIFT) |
+                                    (chanmode << ATIM_CCMR2_OC4M_SHIFT) |
+                                    TIM_CCMR2_OC4PE;
+
+                        // Set the duty cycle by writing to the CCR register for this channel
+                        Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCR4_OFFSET, ccr);
+                        break;
+                    default:
+                        continue;
+                }
+            }
+
+            // Disable the Channel by resetting the CCxE Bit in the CCER register
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCER_OFFSET, out uint ccer);
+            ccer &= ~ccenable;
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCER_OFFSET, ccer);
+
+            // Fetch the CR2, CCMR1, and CCMR2 register (already have cr1 and ccer)
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR2_OFFSET, out uint cr2);
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCMR1_OFFSET, out uint ccmr1);
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCMR2_OFFSET, out uint ccmr2);
+
+            // Reset the Output Compare Mode Bits and set the select output compare mode
+            ccmr1 &= ~(TIM_CCMR1_CC1S | TIM_CCMR1_OC1M | TIM_CCMR1_OC1PE |
+                       TIM_CCMR2_CC3S | TIM_CCMR1_OC2M | TIM_CCMR1_OC2PE);
+            ccmr2 &= ~(TIM_CCMR2_CC3S | TIM_CCMR2_OC3M | TIM_CCMR2_OC3PE |
+                       TIM_CCMR2_CC4S | TIM_CCMR2_OC4M | TIM_CCMR2_OC4PE);
+            ccmr1 |= ocmode1;
+            ccmr2 |= ocmode2;
+
+            // Reset the output polarity level of all channels (selects high polarity)
+            ccer &= ~(TIM_CCER_CC1P | TIM_CCER_CC2P | TIM_CCER_CC3P | TIM_CCER_CC4P);
+
+            // Enable the output state of the selected channels
+            ccer &= ~(TIM_CCER_CC1E | TIM_CCER_CC2E | TIM_CCER_CC3E | TIM_CCER_CC4E);
+            ccer |= ccenable;
+
+            // CCxNP must be cleared in any case
+            ccer &= ~(TIM_CCER_CC1NP | TIM_CCER_CC2NP | TIM_CCER_CC3NP | TIM_CCER_CC4NP);
+
+            // Save the modified register values
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR2_OFFSET, cr2);
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCMR1_OFFSET, ccmr1);
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCMR2_OFFSET, ccmr2);
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CCER_OFFSET, ccer);
+
+            // Set the ARR Preload Bit
+            Nuttx.TryGetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR1_OFFSET, out cr1);
+            cr1 |= TIM_CR1_ARPE;
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR1_OFFSET, cr1);
+
+            // Just enable the timer, leaving all interrupts disabled
+            cr1 |= TIM_CR1_CEN;
+            Nuttx.SetRegister(DriverHandle, TIM3_BASE_ADDRESS + TIMx_CR1_OFFSET, cr1);
 
             Console.WriteLine("-EnablePC6PWM");
         }
