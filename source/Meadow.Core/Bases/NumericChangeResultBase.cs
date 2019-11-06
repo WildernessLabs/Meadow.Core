9ec93a8c48a65e9debd12ed8914b409c36b99e4a
Bryan Costanich <b@wowzer.net>
2019-11-05 18:55:58 -0800

    docs

--- source/Meadow.Core/Bases/NumericChangeResultBase.cs
+++ source/Meadow.Core/Bases/NumericChangeResultBase.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 namespace Meadow
 {
+    /// <summary>
+    /// Base class for event data that is numeric based and contains new and old
+    /// value.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class NumericChangeResultBase<T> : INumericChangeResult<T>
     {
+        /// <summary>
+        /// Current/new event value.
+        /// </summary>
         public T New { get; set; }
+        /// <summary>
+        /// Previous value.
+        /// </summary>
         public T Old { get; set; }
 
+        /// <summary>
+        /// Change in value between `New` and `Old`.
+        /// </summary>
         public abstract T Delta { get; }
+        /// <summary>
+        /// Change in value, as a percentage, between `New` and `Old`.
+        /// </summary>
         public abstract T DeltaPercent { get; }
 
+        /// <summary>
+        /// Creates a new `NumericChangeResultBase`.
+        /// </summary>
+        /// <param name="newValue">Current event value.</param>
+        /// <param name="oldValue">Previous event value.</param>
         public NumericChangeResultBase(T newValue, T oldValue)
         {
             New = newValue;
--- source/Meadow.Core/Contracts/IChangeResult.cs
+++ source/Meadow.Core/Contracts/IChangeResult.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 namespace Meadow
 {
+    /// <summary>
+    /// Contract for change notifications.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IChangeResult<T>
     {
+        /// <summary>
+        /// Current/new event value.
+        /// </summary>
         T New { get; set; }
+        /// <summary>
+        /// Previous value.
+        /// </summary>
         T Old { get; set; }
+        /// <summary>
+        /// Change in value between `New` and `Old`.
+        /// </summary>
         T Delta { get; }
     }
 
+    /// <summary>
+    /// Contract for change notifications, where the values are numeric.
+    /// </summary>
+    /// <typeparam name="T">The type, such as a float or int.</typeparam>
     public interface INumericChangeResult<T> : IChangeResult<T>
     {
+        /// <summary>
+        /// Change in value, as a percentage, between `New` and `Old`.
+        /// </summary>
         T DeltaPercent { get; }
     }
 }
