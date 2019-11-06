8a799e4b60c8845e82115cbe328c39bcc3138615
Bryan Costanich <b@wowzer.net>
2019-11-06 11:44:12 -0800

    better interface and more docs

--- source/Meadow.Core/Peripherals/Sensors/Moisture/IMoistureSensor.cs
+++ source/Meadow.Core/Peripherals/Sensors/Moisture/IMoistureSensor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Meadow.Peripherals.Sensors.Moisture
 {
@@ -13,5 +14,33 @@ namespace Meadow.Peripherals.Sensors.Moisture
         /// Raised when a change in moisture is detected.
         /// </summary>
         event EventHandler<FloatChangeResult> Changed;
+
+        /// <summary>
+        /// Convenience method to get the current soil moisture. For frequent reads, use
+        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
+        /// </summary>
+        /// <param name="sampleCount">The number of sample readings to take. 
+        /// must be greater than 0.</param>
+        /// <param name="sampleInterval">The interval, in milliseconds, between
+        /// sample readings.</param>
+        /// <returns></returns>
+        Task<float> Read(int sampleCount = 10, int sampleInterval = 40);
+
+        /// <summary>
+        /// Starts continuously sampling the temperature. Also triggers the
+        /// events to fire, and IObservable subscribers to get notified.
+        /// </summary>
+        /// <param name="sampleCount"></param>
+        /// <param name="sampleIntervalDuration"></param>
+        /// <param name="sampleSleepDuration"></param>
+        void StartUpdating(
+            int sampleCount = 10,
+            int sampleIntervalDuration = 40,
+            int sampleSleepDuration = 0);
+
+        /// <summary>
+        /// Stops sampling the temperature.
+        /// </summary>
+        void StopUpdating();
     }
 }
