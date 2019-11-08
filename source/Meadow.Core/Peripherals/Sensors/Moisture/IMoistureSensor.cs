b1cdad002b1bfff1f269ef4f8e726df0c3c32ce4
Bryan Costanich <b@wowzer.net>
2019-11-07 16:28:49 -0800

    removed methods that are specific to analog sensors.

--- source/Meadow.Core/Peripherals/Sensors/Moisture/IMoistureSensor.cs
+++ source/Meadow.Core/Peripherals/Sensors/Moisture/IMoistureSensor.cs
@@ -15,39 +15,39 @@ namespace Meadow.Peripherals.Sensors.Moisture
         /// </summary>
         event EventHandler<FloatChangeResult> MoistureChanged;
 
-        /// <summary>
-        /// Convenience method to get the current soil moisture. For frequent reads, use
-        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
-        /// </summary>
-        /// <param name="sampleCount">The number of sample readings to take. 
-        /// must be greater than 0.</param>
-        /// <param name="sampleInterval">The interval, in milliseconds, between
-        /// sample readings.</param>
-        /// <returns></returns>
-        Task<float> Read(int sampleCount = 10, int sampleInterval = 40);
+        ///// <summary>
+        ///// Convenience method to get the current soil moisture. For frequent reads, use
+        ///// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
+        ///// </summary>
+        ///// <param name="sampleCount">The number of sample readings to take. 
+        ///// must be greater than 0.</param>
+        ///// <param name="sampleInterval">The interval, in milliseconds, between
+        ///// sample readings.</param>
+        ///// <returns></returns>
+        //Task<float> Read(int sampleCount = 10, int sampleInterval = 40);
 
-        /// <summary>
-        /// Starts continuously sampling the sensor.
-        ///
-        /// This method also starts raising `Changed` events and IObservable
-        /// subscribers getting notified. Use the `readIntervalDuration` parameter
-        /// to specify how often events and notifications are raised/sent.
-        /// </summary>
-        /// <param name="sampleCount">How many samples to take during a given
-        /// reading. These are automatically averaged to reduce noise.</param>
-        /// <param name="sampleIntervalDuration">The time, in milliseconds,
-        /// to wait in between samples during a reading.</param>
-        /// <param name="readIntervalDuration">The time, in milliseconds, to wait
-        /// between sets of sample readings. This value determines how often
-        /// `Changed` events are raised and `IObservable` consumers are notified.</param>
-        void StartUpdating(
-            int sampleCount = 10,
-            int sampleIntervalDuration = 40,
-            int readIntervalDuration = 1000);
+        ///// <summary>
+        ///// Starts continuously sampling the sensor.
+        /////
+        ///// This method also starts raising `Changed` events and IObservable
+        ///// subscribers getting notified. Use the `readIntervalDuration` parameter
+        ///// to specify how often events and notifications are raised/sent.
+        ///// </summary>
+        ///// <param name="sampleCount">How many samples to take during a given
+        ///// reading. These are automatically averaged to reduce noise.</param>
+        ///// <param name="sampleIntervalDuration">The time, in milliseconds,
+        ///// to wait in between samples during a reading.</param>
+        ///// <param name="readIntervalDuration">The time, in milliseconds, to wait
+        ///// between sets of sample readings. This value determines how often
+        ///// `Changed` events are raised and `IObservable` consumers are notified.</param>
+        //void StartUpdating(
+        //    int sampleCount = 10,
+        //    int sampleIntervalDuration = 40,
+        //    int readIntervalDuration = 1000);
 
-        /// <summary>
-        /// Stops sampling the moisture level.
-        /// </summary>
-        void StopUpdating();
+        ///// <summary>
+        ///// Stops sampling the moisture level.
+        ///// </summary>
+        //void StopUpdating();
     }
 }
--- source/Meadow.Core/Peripherals/Sensors/Temperature/ITemperatureSensor.cs
+++ source/Meadow.Core/Peripherals/Sensors/Temperature/ITemperatureSensor.cs
@@ -21,40 +21,40 @@ namespace Meadow.Peripherals.Temperature
         /// </summary>
         event EventHandler<FloatChangeResult> TemperatureChanged;
 
-        /// <summary>
-        /// Convenience method to get the current temperature. For frequent reads, use
-        /// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
-        /// </summary>
-        /// <param name="sampleCount">The number of sample readings to take. 
-        /// Must be greater than 0. These samples are automatically averaged.</param>
-        /// <param name="sampleIntervalDuration">The time, in milliseconds,
-        /// to wait in between samples during a reading.</param>
-        /// <returns>A float value that's ann average value of all the samples taken.</returns>
-        Task<float> ReadTemperature(int sampleCount = 10, int sampleIntervalDuration = 40);
+        ///// <summary>
+        ///// Convenience method to get the current temperature. For frequent reads, use
+        ///// StartSampling() and StopSampling() in conjunction with the SampleBuffer.
+        ///// </summary>
+        ///// <param name="sampleCount">The number of sample readings to take. 
+        ///// Must be greater than 0. These samples are automatically averaged.</param>
+        ///// <param name="sampleIntervalDuration">The time, in milliseconds,
+        ///// to wait in between samples during a reading.</param>
+        ///// <returns>A float value that's ann average value of all the samples taken.</returns>
+        //Task<float> ReadTemperature(int sampleCount = 10, int sampleIntervalDuration = 40);
 
-        /// <summary>
-        /// Starts continuously sampling the sensor.
-        ///
-        /// This method also starts raising `Changed` events and IObservable
-        /// subscribers getting notified. Use the `readIntervalDuration` parameter
-        /// to specify how often events and notifications are raised/sent.
-        /// </summary>
-        /// <param name="sampleCount">How many samples to take during a given
-        /// reading. These are automatically averaged to reduce noise.</param>
-        /// <param name="sampleIntervalDuration">The time, in milliseconds,
-        /// to wait in between samples during a reading.</param>
-        /// <param name="readIntervalDuration">The time, in milliseconds, to wait
-        /// between sets of sample readings. This value determines how often
-        /// `Changed` events are raised and `IObservable` consumers are notified.</param>
-        void StartUpdating(
-            int sampleCount = 10,
-            int sampleIntervalDuration = 40,
-            int readIntervalDuration = 1000);
+        ///// <summary>
+        ///// Starts continuously sampling the sensor.
+        /////
+        ///// This method also starts raising `Changed` events and IObservable
+        ///// subscribers getting notified. Use the `readIntervalDuration` parameter
+        ///// to specify how often events and notifications are raised/sent.
+        ///// </summary>
+        ///// <param name="sampleCount">How many samples to take during a given
+        ///// reading. These are automatically averaged to reduce noise.</param>
+        ///// <param name="sampleIntervalDuration">The time, in milliseconds,
+        ///// to wait in between samples during a reading.</param>
+        ///// <param name="readIntervalDuration">The time, in milliseconds, to wait
+        ///// between sets of sample readings. This value determines how often
+        ///// `Changed` events are raised and `IObservable` consumers are notified.</param>
+        //void StartUpdating(
+        //    int sampleCount = 10,
+        //    int sampleIntervalDuration = 40,
+        //    int readIntervalDuration = 1000);
 
-        /// <summary>
-        /// Stops sampling the temperature.
-        /// </summary>
-        void StopUpdating();
+        ///// <summary>
+        ///// Stops sampling the temperature.
+        ///// </summary>
+        //void StopUpdating();
 
     }
 }
