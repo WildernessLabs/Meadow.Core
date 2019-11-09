245a887cb08d9b90a889a8263eaca47f4fecfc41
ctacke <ctacke@opennetcf.com>
2019-11-09 09:30:08 -0600

    hunting a serial killer

---
+++ source/Tests/SerialLoopback/MeadowApp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+
+namespace SerialLoopback
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        public MeadowApp()
+        {
+            Console.WriteLine("+SerialLoopback");
+
+            Run();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Using 'ttyS1'...");
+            var port = new SerialPort("ttyS1", 115200);
+            Console.WriteLine("\tCreated");
+            port.Open();
+            if (port.IsOpen)
+            {
+                Console.WriteLine("\tOpened");
+            }
+            else
+            {
+                Console.WriteLine("\tFailed to Open");
+            }
+
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                Console.WriteLine("Writing data...");
+                port.Write(Encoding.ASCII.GetBytes("Hello Meadow!"));
+
+                var read = port.Read(buffer, 0, buffer.Length);
+
+                if (read == 0)
+                {
+                    Console.WriteLine($"Read {read} bytes");
+                }
+                else
+                {
+                    Console.WriteLine($"Read {read} bytes: {BitConverter.ToString(buffer, 0, read)}");
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
