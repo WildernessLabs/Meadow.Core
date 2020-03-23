ff5d86964349825451d6c6eb2d8d3ed612654685
Adrian Stevens <adrian.stevens@gmail.com>
2020-03-22 21:00:13 -0700

    Add clock test app

---
+++ source/Tests/RealTimeClock/MeadowApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+
+        public MeadowApp()
+        {
+            //set current time to 12pm on March 20, 2020
+            Console.WriteLine("Hello RTC");
+            
+            Device.SetClock(new DateTime(2020, 3, 22, 12, 0, 0));
+
+            Console.WriteLine($"Today is: {DateTime.Now}");
+        }
+    }
+}
