ee81fb71d9cc66e6ecf95518b01c98a6c4dfbe97
bryan costanich <b@wowzer.net>
2020-06-27 10:58:50 -0700

    Basic File Test app

---
+++ source/Tests/Basic_FileSystem/MeadowApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+
+namespace MeadowApp
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        RgbPwmLed onboardLed;
+
+        public MeadowApp()
+        {
+            string appRootDir = "meadow0";
+
+
+            // This works
+            //string[] files = Directory.GetFiles("/");
+            string[] files = Directory.GetFiles(appRootDir);
+            foreach (var file in files) {
+                Console.WriteLine($"File: {file}");
+            }
+
+            // this fails with `Unhandled Exception: System.IO.FileNotFoundException: Could not find file '/dev'.`
+            //DirectoryInfo directoryInfo = new DirectoryInfo("/");
+            //FileInfo[] f = directoryInfo.GetFiles();
+            //foreach (FileInfo file in f) {
+            //    Console.WriteLine("File Name: {0} Size: {1}", file.Name, file.Length);
+            //}
+
+
+        }
+
+    }
+}
