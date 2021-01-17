245cfecc0b14f9a6dd0aa89f516fa889988f26d7
bryan costanich <b@wowzer.net>
2021-01-17 11:02:49 -0800

    Initial unit library code with temp and tests

---
+++ source/Meadow.Units/Conversions/TempConversions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Meadow.Units.Conversions
+{
+    internal static class TempConversions
+    {
+        public static Func<double, double> FToC = (value) => (value - 32D) * (5D / 9D);
+        public static Func<double, double> FToK = (value) => (value - 32D) * (5D / 9D) + 273.15D;
+        public static Func<double, double> CToF = (value) => value * (9D / 5D) + 32D;
+        public static Func<double, double> CToK = (value) => value + 273.15D;
+        public static Func<double, double> KToC = (value) => value - 273.15D;
+        public static Func<double, double> KToF = (value) => (value - 273.15D) * (9D / 5D) + 32D;
+    }
+}
