f997780fd49cb34e7f427ea2595dc64d19a1610c
bryan costanich <b@wowzer.net>
2021-02-12 17:41:37 -0800

    Added Azimuth to units

---
+++ source/Meadow.Units.Tests/AzimuthTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using Meadow.Units;
+using static Meadow.Units.Tests.Helpers;
+
+namespace Meadow.Units.Tests
+{
+    [TestFixture()]
+    public class AzimuthTests
+    {
+        [Test()]
+        public void AzimuthCtors()
+        {
+            Azimuth a = new Azimuth(0d);
+            Assert.That(a.DecimalDegrees == 0);
+
+            Azimuth a2 = new Azimuth();
+            Assert.That(a.DecimalDegrees == 0);
+
+            Azimuth a3 = Azimuth.FromCompass16PointCardinalName(Compass16PointCardinalNames.E);
+            Assert.That(a3.Compass16PointCardinalName == Compass16PointCardinalNames.E);
+
+            Azimuth v4 = Azimuth.FromDecimalDegrees(270);
+            Assert.That(v4.DecimalDegrees == 270);
+        }
+
+        [Test()]
+        public void AzimuthConversions()
+        {
+            Azimuth v = new Azimuth(90);
+            Assert.That(v.Compass16PointCardinalName == Compass16PointCardinalNames.E);
+
+            Azimuth v2 = new Azimuth(Compass16PointCardinalNames.NNW);
+            Assert.That(v2.DecimalDegrees == 337.5);
+
+            Azimuth v3 = new Azimuth(191.25);
+            Assert.That(v3.Compass16PointCardinalName == Compass16PointCardinalNames.SSW);
+
+        }
+    }
+}
---
+++ source/Meadow.Units/CardinalDirection.Compass16PointCardinalNames.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Meadow.Units
+{
+    public enum Compass16PointCardinalNames
+    {
+        N,
+        NNE,
+        NE,
+        ENE,
+        E,
+        ESE,
+        SE,
+        SSE,
+        S,
+        SSW,
+        SW,
+        WSW,
+        W,
+        WNW,
+        NW,
+        NNW
+    }
+}
