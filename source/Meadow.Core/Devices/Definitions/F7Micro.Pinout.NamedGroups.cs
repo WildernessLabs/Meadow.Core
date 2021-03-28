8803a9cfce66fb6d427c976eb0f8c983cf7f3f46
bryan costanich <b@wowzer.net>
2021-03-27 21:55:13 -0700

    pretty sure we don't use these anywhere.

--- source/Meadow.Core/Devices/Definitions/F7Micro.Pinout.NamedGroups.cs
+++ source/Meadow.Core/Devices/Definitions/F7Micro.Pinout.NamedGroups.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using Meadow.Hardware;
+﻿//using System;
+//using System.Collections.Generic;
+//using Meadow.Hardware;
 
-namespace Meadow.Devices
-{
-    public partial class F7Micro
-    {
-        public partial class Pinout
-        {
-            // TODO: are these even used? if not, let's delete.
-            // also; COM4 is missing, which makes me think we're not even using these.
-            public class F7NamedPinGroups : INamedPinGroups
-            {
-                protected Pinout _parent;
+//namespace Meadow.Devices
+//{
+//    public partial class F7Micro
+//    {
+//        public partial class Pinout
+//        {
+//            // TODO: are these even used? if not, let's delete.
+//            // also; COM4 is missing, which makes me think we're not even using these.
+//            public class F7NamedPinGroups : INamedPinGroups
+//            {
+//                protected Pinout _parent;
 
-                public IList<NamedPinGroup> AllGroups { get; } = new List<NamedPinGroup>();
+//                public IList<NamedPinGroup> AllGroups { get; } = new List<NamedPinGroup>();
 
-                public NamedPinGroup Com1;
-                public NamedPinGroup Spi1;
-                public NamedPinGroup I2c1;
+//                public NamedPinGroup Com1;
+//                public NamedPinGroup Spi1;
+//                public NamedPinGroup I2c1;
 
-                public F7NamedPinGroups(Pinout parentDefinitions)
-                {
-                    _parent = parentDefinitions;
-                    this.InitNamedPinGroups();
-                }
+//                public F7NamedPinGroups(Pinout parentDefinitions)
+//                {
+//                    _parent = parentDefinitions;
+//                    this.InitNamedPinGroups();
+//                }
 
-                protected void InitNamedPinGroups()
-                {
-                    Com1 = new NamedPinGroup("COM1", new IPin[] { _parent.D12, _parent.D13 });
-                    Spi1 = new NamedPinGroup("SPI1", new IPin[] { _parent.SCK, _parent.MOSI, _parent.MISO });
-                    I2c1 = new NamedPinGroup("I2C1", new IPin[] { _parent.D08, _parent.D07 });
-                }
+//                protected void InitNamedPinGroups()
+//                {
+//                    Com1 = new NamedPinGroup("COM1", new IPin[] { _parent.D12, _parent.D13 });
+//                    Spi1 = new NamedPinGroup("SPI1", new IPin[] { _parent.SCK, _parent.MOSI, _parent.MISO });
+//                    I2c1 = new NamedPinGroup("I2C1", new IPin[] { _parent.D08, _parent.D07 });
+//                }
 
-            }
-        }
-    }
-}
+//            }
+//        }
+//    }
+//}
