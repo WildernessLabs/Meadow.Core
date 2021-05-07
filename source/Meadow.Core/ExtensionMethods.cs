ca40c2bedc0dde20d3f9099716fcf94c9cb4dfd8
bryan costanich <b@wowzer.net>
2021-05-06 20:33:21 -0700

    Added `.Map()` extension methods for int, float, double

--- source/Meadow.Core/ExtensionMethods.cs
+++ source/Meadow.Core/ExtensionMethods.cs
@@ -75,5 +75,53 @@ namespace Meadow
             // if we go here, doesn't exist
             return -1;
         }
+
+        /// <summary>
+        /// Maps a source value's position within a range of numbers to the same positon
+        /// within a another range of numbers. For instance, will map a source value of `30`
+        /// in the range of `0` to `100` to a value of `0.3` in a given range of `0.0` to `1.0`.
+        /// </summary>
+        /// <param name="souceValue">The value to map to the new domain.</param>
+        /// <param name="sourceMin">The minimum value of the source domain.</param>
+        /// <param name="sourceMax">The maximum value of the source domain.</param>
+        /// <param name="targetMin">The minimum value of the destinatino domain.</param>
+        /// <param name="targetMax">The maximum value of the destination domain.</param>
+        /// <returns></returns>
+        static public float Map(this float souceValue, float sourceMin, float sourceMax, float targetMin, float targetMax)
+        {
+            return (souceValue - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
+        }
+
+        /// <summary>
+        /// Maps a source value's position within a range of numbers to the same positon
+        /// within a another range of numbers. For instance, will map a source value of `30`
+        /// in the range of `0` to `100` to a value of `0.3` in a given range of `0.0` to `1.0`.
+        /// </summary>
+        /// <param name="souceValue">The value to map to the new domain.</param>
+        /// <param name="sourceMin">The minimum value of the source domain.</param>
+        /// <param name="sourceMax">The maximum value of the source domain.</param>
+        /// <param name="targetMin">The minimum value of the destinatino domain.</param>
+        /// <param name="targetMax">The maximum value of the destination domain.</param>
+        /// <returns></returns>
+        static public double Map(this double souceValue, double sourceMin, double sourceMax, double targetMin, double targetMax)
+        {
+            return (souceValue - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
+        }
+
+        /// <summary>
+        /// Maps a source value's position within a range of numbers to the same positon
+        /// within a another range of numbers. For instance, will map a source value of `30`
+        /// in the range of `0` to `100` to a value of `0.3` in a given range of `0.0` to `1.0`.
+        /// </summary>
+        /// <param name="souceValue">The value to map to the new domain.</param>
+        /// <param name="sourceMin">The minimum value of the source domain.</param>
+        /// <param name="sourceMax">The maximum value of the source domain.</param>
+        /// <param name="targetMin">The minimum value of the destinatino domain.</param>
+        /// <param name="targetMax">The maximum value of the destination domain.</param>
+        /// <returns></returns>
+        static public int Map(this int souceValue, int sourceMin, int sourceMax, int targetMin, int targetMax)
+        {
+            return (souceValue - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
+        }
     }
 }
