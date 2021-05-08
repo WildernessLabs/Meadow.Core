1c77334fb1ac8c3c2f67dfe3a8b32d61d00949c1
bryan costanich <b@wowzer.net>
2021-05-07 20:40:25 -0700

    Added `IWindVane`

---
+++ source/Meadow.Core/Peripherals/Sensors/Weather/IWindVane.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Meadow.Units;
+
+namespace Meadow.Peripherals.Sensors.Weather
+{
+    public interface IWindVane : ISensor
+    {
+        /// <summary>
+        /// Raised when the azimuth of the wind changes.
+        /// </summary>
+        event EventHandler<IChangeResult<Azimuth>> Updated;
+
+        /// <summary>
+        /// The last recorded azimuth of the wind.
+        /// </summary>
+        Azimuth? WindAzimuth { get;}
+    }
+}
