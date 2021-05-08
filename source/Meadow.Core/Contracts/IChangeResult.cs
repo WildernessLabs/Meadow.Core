ed3d191aab7006e8e6309241c050e5a590628be3
bryan costanich <b@wowzer.net>
2021-05-07 19:23:07 -0700

    cleanup of old changeresults and such

--- source/Meadow.Core/Contracts/IChangeResult.cs
+++ source/Meadow.Core/Contracts/IChangeResult.cs
@@ -5,10 +5,7 @@ namespace Meadow
     /// Contract for change notifications.
     /// </summary>
     /// <typeparam name="UNIT"></typeparam>
-    public interface IChangeResult<UNIT>
-        //where T : notnull //struct
-        //where T: notnull//, IComparable //System.Collections.IStructuralEquatable, System.Collections.IStructuralComparable,
-        where UNIT: struct
+    public interface IChangeResult<UNIT> where UNIT: struct
     {
         /// <summary>
         /// Current/new event value.
@@ -18,30 +15,5 @@ namespace Meadow
         /// Previous value.
         /// </summary>
         UNIT? Old { get; set; }
-        ///// <summary>
-        ///// Change in value between `New` and `Old`.
-        ///// </summary>
-        //object Delta { get; }
-    }
-
-    /// <summary>
-    /// Contract for change notifications, where the values are numeric.
-    /// </summary>
-    /// <typeparam name="UNIT">The type, such as a float or int.</typeparam>
-    public interface INumericChangeResult<UNIT> : IChangeResult<UNIT>
-        where UNIT : struct
-        //where T : notnull//, IComparable// System.Collections.IStructuralEquatable, System.Collections.IStructuralComparable, 
-    {
-        /// <summary>
-        /// Change in value, as a percentage, between `New` and `Old`.
-        /// </summary>
-        UNIT? DeltaPercent { get; }
-
-        UNIT? Delta {get;}
-    }
-
-    public interface ITimeChangeResult : IChangeResult<DateTime>
-    {
-        TimeSpan? Delta { get; }
     }
 }
--- source/Meadow.Core/Hardware/DigitalInputPortEventArgs.cs
+++ source/Meadow.Core/Hardware/DigitalInputPortEventArgs.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 namespace Meadow.Hardware
 {
+    //TODO: should this inherit from ChangeResult<DateTime> and not eventargs?
+    // eventargs is a class, and our change results are structs.
     /// <summary>
     /// Provides data for events that come from an IDigitalInputPort.
     /// </summary>
-    //public class DigitalInputPortEventArgs : EventArgs, IChangeResult<DateTime>
-    public class DigitalInputPortEventArgs : EventArgs, ITimeChangeResult
+    public class DigitalInputPortEventArgs : EventArgs, IChangeResult<DateTime> //, ITimeChangeResult
     {
         public bool Value { get; set; }
         public DateTime New { get; set; }
         public DateTime? Old { get; set; }
 
         public TimeSpan? Delta { get { return New - Old; } }
-        //public DateTime Delta { get { return DateTime.MinValue.Add(New - Old); } }
 
         public DigitalInputPortEventArgs() { }
 
