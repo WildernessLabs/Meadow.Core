4b104a4fe4f5c86a8536fff1bb0890ccb3db1b20
Chris Tacke <christopher.tacke@hypergiant.com>
2021-05-09 09:07:55 -0500

    Doc updates/separated files

--- source/Meadow.Core/Peripherals/Sensors/Atmospheric/IVocSensor.cs
+++ source/Meadow.Core/Peripherals/Sensors/Atmospheric/IVocSensor.cs
@@ -4,20 +4,20 @@ using System;
 namespace Meadow.Peripherals.Sensors
 {
     /// <summary>
-    /// CO2 sensor interface requirements.
+    /// Volatile Organic Compound (VOC) sensor interface requirements.
     /// </summary>
-    public interface ICO2Sensor : ISensor
+    public interface IVocSensor : ISensor
     {
         /// <summary>
-        /// Last value read from the CO2 sensor.
+        /// Last value read from the VOC sensor.
         /// </summary>
-        Concentration? CO2 { get; }
+        Concentration? VOC { get; }
 
         /// <summary>
         /// Raised when a new reading has been made. Events will only be raised
         /// while the driver is updating. To start, call the `StartUpdating()`
         /// method.
         /// </summary>
-        event EventHandler<ChangeResult<Concentration>> CO2Updated;
+        event EventHandler<ChangeResult<Concentration>> VOCUpdated;
     }
 }
