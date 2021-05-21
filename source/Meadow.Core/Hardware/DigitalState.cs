583d0bffba66c34dccd2962bf1cffba7d519e088
bryan costanich <b@wowzer.net>
2021-05-21 12:08:04 -0700

    Moved `DigitalState` into its own file and renamed `DigitalInputPortEventArgs` to the updated class name of `DigitalPortResult`

---
+++ source/Meadow.Core/Hardware/DigitalState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Meadow.Hardware
+{
+    /// <summary>
+    /// Represents a snapshot of the state of a digital port at a given time.
+    /// </summary>
+    public struct DigitalState
+    {
+        /// <summary>
+        /// The state of the port at the time of the event or notification.
+        /// `true` == `HIGH`. `false` == `LOW`
+        /// </summary>
+        public bool State { get; set; }
+        /// <summary>
+        /// The time at the event or notification.
+        /// </summary>
+        public DateTime Time { get; set; }
+
+        public DigitalState(bool state, DateTime time)
+        {
+            State = state;
+            Time = time;
+        }
+    }
+}
