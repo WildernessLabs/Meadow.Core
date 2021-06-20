7d2a77310546b3739dac22dee6fe447f21b4940f
bryan costanich <b@wowzer.net>
2021-06-19 21:35:39 -0700

    Hiding sausage

--- source/Meadow.Contracts/Peripherals/Sensors/Rotary/IRotaryEncoder.cs
+++ source/Meadow.Contracts/Peripherals/Sensors/Rotary/IRotaryEncoder.cs
@@ -9,16 +9,6 @@ namespace Meadow.Peripherals.Sensors.Rotary
     public interface IRotaryEncoder
     {
         /// <summary>
-        /// Raised when the rotary encoder is rotated and returns a RotaryTurnedEventArgs object which describes the direction of rotation.
-        /// </summary>
-        IDigitalInputPort APhasePort { get; }
-
-        /// <summary>
-        /// Returns the pin connected to the B-phase output on the rotary encoder.
-        /// </summary>
-        IDigitalInputPort BPhasePort { get; }
-
-        /// <summary>
         /// Gets the last direction of rotation
         /// </summary>
         RotationDirection? LastDirectionOfRotation { get; }
