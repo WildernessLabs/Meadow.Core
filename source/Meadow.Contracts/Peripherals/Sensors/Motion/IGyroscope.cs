e1d28314d7c6101e96b609c63ab636c631d0b102
bryan costanich <b@wowzer.net>
2021-06-25 13:11:54 -0700

    rename `IAngularAccelerometer` to `IGyroscope`

--- source/Meadow.Contracts/Peripherals/Sensors/Motion/IGyroscope.cs
+++ source/Meadow.Contracts/Peripherals/Sensors/Motion/IGyroscope.cs
@@ -6,7 +6,7 @@ namespace Meadow.Peripherals.Sensors.Motion
     /// <summary>
     /// Represents a generic accelerometer sensor.
     /// </summary>
-    public interface IAngularAccelerometer : ISensor
+    public interface IGyroscope : ISensor
     {
         /// <summary>
         /// Last value read from the Temperature sensor.
