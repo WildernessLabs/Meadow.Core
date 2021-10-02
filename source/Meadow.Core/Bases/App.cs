cef5069f8bec0707abe832d659b648a5ec6373b8
bryan costanich <b@wowzer.net>
2021-10-02 11:30:03 -0700

    App re-arch
    * added a call to MeadowOS.Initialize() to the App ctor.
    * Moved synchronization context setting to MeadowOS
    * organized class a bit
    * cleaned up notes
    
    Tested a bunch of the inherited ctor stuff, and it looks like we our worries about the base app ctor not running, but in all tested cases, it always get executed.

--- source/Meadow.Core/Bases/App.cs
+++ source/Meadow.Core/Bases/App.cs
@@ -13,73 +13,85 @@ namespace Meadow
         where A : class, IApp
         where D : class, IMeadowDevice
     {
+        //==== public properties
         /// <summary>
         /// </summary>
-        /// <value>The current.</value>
-        public static A Current
-        {
+        /// <value>The current instance.</value>
+        public static A Current {
             get { return _current; }
-        } private static A _current = null!;
-
-        private static SynchronizationContext _mainContext { get; }
-
-        static App()
-        {
-            AppDomain.CurrentDomain.UnhandledException += StaticOnUnhandledException;
-
-            _mainContext = new MeadowSynchronizationContext();
-            SynchronizationContext.SetSynchronizationContext(_mainContext);
         }
+        private static A _current = null!;
 
-        protected App()
-        {
-            _device = Activator.CreateInstance<D>();
-
-            MeadowOS.SetSynchronizationContext(_mainContext);
-
-            _device.Initialize();
-            MeadowOS.CurrentDevice = _device;
-
-            // initialize file system folders and such
-            MeadowOS.InitializeFileSystem();
-
-            //BUGBUG: because a user's `App` class doesn't have to call this
-            // base ctor, then this might not ever run, or it might run
-            // non-deterministically. so we need to figure out how to make sure
-            // this stuff happens
-            _current = this as A ?? throw new Exception($"Unable to convert to {typeof(A).Name}");
-        }
-
-        public static D Device
-        {
+        /// <summary>
+        /// The Device
+        /// </summary>
+        public static D Device {
             get => _device;
-        } private static D _device = null!;
+        }
+        private static D _device = null!;
 
         /// <summary>
         /// Called when the application is put to sleep.
         /// </summary>
-        public virtual void WillSleep() {}
+        public virtual void WillSleep() { }
 
         /// <summary>
         /// Called when the application wakes up from sleep.
         /// </summary>
-        public virtual void OnWake() {}
+        public virtual void OnWake() { }
 
         /// <summary>
         /// 
         /// </summary>
-        public virtual void WillReset() {}
+        public virtual void WillReset() { }
+
+        /// <summary>
+        /// this is fine.
+        /// </summary>
+        static App()
+        {
+            // capture unhandled exceptions so the runtime doesn't tear itself
+            // apart
+            //
+            // BC NOTE: I tried to move this to the MeadowOS class but we have a
+            // chicken/egg problem. If MeadowOS isn't initialized we throw an err,
+            // but because it's not instantiated, the exception handler won't exist.
+            AppDomain.CurrentDomain.UnhandledException += StaticOnUnhandledException;
+
+            // initialze MeadowOS
+            MeadowOS.Initialize();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected App()
+        {
+            // BC Note: near as i can tell, in C# if a default ctor is specified
+            // in a base class, it will be called no matter what, whether the
+            // derived class implicitly calls it (public foo() : base()) or not.
+            // Even if the derived class has no default constructor.
+            //
+            // TL;DR: this will always be called.
+
+            _device = Activator.CreateInstance<D>();
+
+            _device.Initialize();
+            MeadowOS.CurrentDevice = _device;
+
+            // singleton setting
+            _current = this as A ?? throw new Exception($"Unable to convert to {typeof(A).Name}");
+        }
+
+        //==== exception handler
 
         private static void StaticOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            if(_current == null)
-            {
+            if (_current == null) {
                 // in the event the user never called the base c'tor
                 Console.WriteLine($"Unhandled Exception: {e.ExceptionObject}");
-            }
-            else
-            {
-                (_current as App<D,A>)?.OnUnhandledException(sender, e);
+            } else {
+                (_current as App<D, A>)?.OnUnhandledException(sender, e);
             }
         }
 
