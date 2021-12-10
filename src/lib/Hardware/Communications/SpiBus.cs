6a120515d39f3975580751ecddfc26941db1653c
ctacke <ctacke@opennetcf.com>
2021-12-10 11:54:03 -0600

    Working on SPI for Pi

--- src/lib/Hardware/Communications/SpiBus.cs
+++ src/lib/Hardware/Communications/SpiBus.cs
@@ -1,23 +1,17 @@
-﻿using Meadow.Hardware;
+﻿using Meadow.Devices;
+using Meadow.Hardware;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Meadow
 {
-    public class SpiBus : ISpiBus, IDisposable
+    public partial class SpiBus : ISpiBus, IDisposable
     {
-        private class SpiTransferCommand
-        {
-            public ulong TransmitBuffer { get; set; } // 64-bit pointer to byte array
-            public ulong ReceiveBuffer { get; set; } // 64-bit pointer to byte array
-            public int Length { get; set; }
-            public int SpeedHz { get; set; } = 500000;
-            public short DelayuSec { get; set; } = 0;
-            public byte BitsPerWord { get; set; } = 8;
-            public byte ChipSelectChange { get; set; }
+        private SemaphoreSlim _busSemaphore = new SemaphoreSlim(1, 1);
 
-        }
-
+        private const int TransferIoctl = 0;
         private const int ModeIoctl = 1;
         private const int JustificationIoctl = 2;
         private const int LengthIoctl = 3;
@@ -36,62 +30,87 @@ namespace Meadow
 
         private int DriverHandle { get; set; }
 
-        public long[] SupportedSpeeds => throw new NotImplementedException();
+        public long[] SupportedSpeeds
+        {
+            get => new long[]
+                {
+                    375,
+                    750,
+                    1500,
+                    3000,
+                    6000,
+                    12000,
+                    24000,
+                    48000
+                };
+        }
 
-        public SpiClockConfiguration Configuration => throw new NotImplementedException();
+        public SpiClockConfiguration Configuration { get; private set; }
+
+        internal unsafe SpiBus(int chipSelect, SpiClockConfiguration.ClockPhase phase, SpiClockConfiguration.ClockPolarity polarity, Units.Frequency speed)
+            : this(chipSelect, (SpiMode)((int)phase | (int)polarity), speed)
+        {
+        }
 
         /// <summary>
         /// 
         /// </summary>
         /// <param name="chipSelect">Must be 0 or 1</param>
-        internal unsafe SpiBus(int chipSelect, SpiMode mode = SpiMode.Mode0, int speedHz = 50000)
+        internal unsafe SpiBus(int chipSelect, SpiMode mode, Units.Frequency speed)
         {
             var bitsPerWord = 8;
+            var busNumber = 0;
 
-            var driver = $"/dev/spidev0.{chipSelect}";
+            var driver = $"/dev/spidev{busNumber}.{chipSelect}";
             DriverHandle = Interop.open(driver, Interop.DriverFlags.O_RDWR);
             if (DriverHandle < 0)
             {
                 // TODO: maybe try modprobe here?
-                throw new Exception($"Unable to open driver {driver}");
+                throw new NativeException($"Unable to open driver {driver}. Check your user permissions", Marshal.GetLastWin32Error());
             }
 
             var status = Interop.ioctl(DriverHandle, Interop._IOW('k', ModeIoctl, 8), (byte*)&mode);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPIMode (WR): {status}");
+                throw new NativeException($"Could not set SPIMode (WR): {status}", Marshal.GetLastWin32Error());
             }
 
             status = Interop.ioctl(DriverHandle, Interop._IOR('k', ModeIoctl, 8), (byte*)&mode);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPIMode (RD): {status}");
+                throw new NativeException($"Could not set SPIMode (RD): {status}", Marshal.GetLastWin32Error());
             }
 
             status = Interop.ioctl(DriverHandle, Interop._IOW('k', LengthIoctl, 8), (byte*)&bitsPerWord);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPI bits per word (WR): {status}");
+                throw new NativeException($"Could not set SPI bits per word (WR): {status}", Marshal.GetLastWin32Error());
             }
 
             status = Interop.ioctl(DriverHandle, Interop._IOR('k', LengthIoctl, 8), (byte*)&bitsPerWord);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPI bits per word (RD): {status}");
+                throw new NativeException($"Could not set SPI bits per word (RD): {status}", Marshal.GetLastWin32Error());
             }
 
-
+            int speedHz = Convert.ToInt32(speed.Hertz);
             status = Interop.ioctl(DriverHandle, Interop._IOW('k', LengthIoctl, 32), (byte*)&speedHz);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPI speed (WR): {status}");
+                throw new NativeException($"Could not set SPI speed (WR): {status}", Marshal.GetLastWin32Error());
             }
 
             status = Interop.ioctl(DriverHandle, Interop._IOR('k', LengthIoctl, 32), (byte*)&speedHz);
             if (status < 0)
             {
-                throw new Exception($"Could not set SPI speed (RD): {status}");
+                throw new NativeException($"Could not set SPI speed (RD): {status}", Marshal.GetLastWin32Error());
             }
+
+            Configuration = new SpiClockConfiguration(speed)
+            {
+                Phase = SpiClockConfiguration.ClockPhase.Zero,
+                Polarity = SpiClockConfiguration.ClockPolarity.Normal
+            };
         }
 
         public void Dispose()
@@ -105,17 +124,76 @@ namespace Meadow
 
         public void Read(IDigitalOutputPort? chipSelect, Span<byte> readBuffer, ChipSelectMode csMode = ChipSelectMode.ActiveLow)
         {
-            throw new NotImplementedException();
+            byte[] writeBuffer = new byte[readBuffer.Length];
+            Exchange(chipSelect, writeBuffer, readBuffer, csMode);
         }
 
         public void Write(IDigitalOutputPort? chipSelect, Span<byte> writeBuffer, ChipSelectMode csMode = ChipSelectMode.ActiveLow)
         {
-            throw new NotImplementedException();
+            byte[] readBuffer = new byte[writeBuffer.Length];
+            Exchange(chipSelect, writeBuffer, readBuffer, csMode);
         }
 
-        public void Exchange(IDigitalOutputPort? chipSelect, Span<byte> writeBuffer, Span<byte> readBuffer, ChipSelectMode csMode = ChipSelectMode.ActiveLow)
+        private void DecipherSPIError(int errorCode)
         {
-            throw new NotImplementedException();
+            switch (errorCode)
+            {
+                default:
+                    throw new NativeException($"Communication error.  Error code {(int)errorCode}");
+            }
+        }
+
+        public unsafe void Exchange(IDigitalOutputPort? chipSelect, Span<byte> writeBuffer, Span<byte> readBuffer, ChipSelectMode csMode = ChipSelectMode.ActiveLow)
+        {
+            if (writeBuffer == null) throw new ArgumentNullException("A non-null sendBuffer is required");
+            if (readBuffer == null) throw new ArgumentNullException("A non-null receiveBuffer is required");
+            if (writeBuffer.Length != readBuffer.Length) throw new Exception("Both buffers must be equal size");
+
+            _busSemaphore.Wait();
+
+            try
+            {
+                if (chipSelect != null)
+                {
+                    // activate the chip select
+                    chipSelect.State = csMode == ChipSelectMode.ActiveLow ? false : true;
+                }
+
+                fixed (byte* pWrite = writeBuffer)
+                fixed (byte* pRead = readBuffer)
+                {
+                    var command = new SpiTransferCommand()
+                    {
+                        Length = readBuffer.Length,
+                        TransmitBuffer = (ulong)pWrite, // NOTE: these are always 64-bit for OS compatibility
+                        ReceiveBuffer = (ulong)pRead,
+                        DelayuSec = 0,
+                        BitsPerWord = 0,
+                        SpeedHz = 0,
+                        ChipSelectChange = 0
+                    };
+
+                    //                    Output.WriteLineIf(_showSpiDebug, "+Exchange");
+                    //                    Output.WriteLineIf(_showSpiDebug, $" Sending {writeBuffer.Length} bytes");
+                    var length = readBuffer.Length;
+                    var status = Interop.ioctl(DriverHandle, Interop._IOW('k', TransferIoctl, 1), (byte*)&length);
+                    if (status != 0)
+                    {
+                        DecipherSPIError(Marshal.GetLastWin32Error());
+                    }
+                    //                    Output.WriteLineIf(_showSpiDebug, $" Received {readBuffer.Length} bytes");
+
+                    if (chipSelect != null)
+                    {
+                        // deactivate the chip select
+                        chipSelect.State = csMode == ChipSelectMode.ActiveLow ? true : false;
+                    }
+                }
+            }
+            finally
+            {
+                _busSemaphore.Release();
+            }
         }
 
         public void SendData(IDigitalOutputPort chipSelect, params byte[] data)
---
+++ src/samples/ST7789_Sample/MeadowApp.cs
@@ -0,0 +1,70 @@
+﻿using Meadow;
+using Meadow.Foundation;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Displays.TftSpi;
+using Meadow.Foundation.Graphics;
+using Meadow.Hardware;
+using System;
+
+namespace PushButton_Sample
+{
+    public class MeadowApp : App<MeadowForLinux<RaspberryPiPinout>, MeadowApp>
+    {
+        private St7789 _display;
+        private MicroGraphics _graphics;
+
+        public MeadowApp()
+        {
+            Console.WriteLine("Initializing...");
+
+            var display = ConfigureDisplay();
+
+            _graphics = new MicroGraphics(display);
+            _graphics.Rotation = RotationType._180Degrees;
+
+            _graphics.Clear(true);
+
+            DoDrawing(_graphics);
+
+            Console.WriteLine("PushButton(s) ready!!!");
+        }
+
+        private IGraphicsDisplay ConfigureDisplay()
+        {
+            var config = new SpiClockConfiguration(
+                new Meadow.Units.Frequency(48, Meadow.Units.Frequency.UnitType.Megahertz), 
+                SpiClockConfiguration.Mode.Mode3);
+
+            var spiBus = Device.CreateSpiBus(
+                Device.Pins.SPI0_SCLK, 
+                Device.Pins.SPI0_MOSI, 
+                Device.Pins.SPI0_MISO, 
+                config);
+
+            _display = new St7789(
+                device: Device,
+                spiBus: spiBus,
+                chipSelectPin: Device.Pins.GPIO25,
+                dcPin: Device.Pins.GPIO27,
+                resetPin: Device.Pins.GPIO22,
+                width: 240, 
+                height: 240, 
+                displayColorMode: ColorType.Format16bppRgb565);
+
+            return _display;
+        }
+
+        private void DoDrawing(MicroGraphics graphics)
+        {
+            graphics.DrawRectangle(120, 0, 120, 220, Color.White, true);
+            graphics.DrawRectangle(0, 0, 120, 20, Color.Red, true);
+            graphics.DrawRectangle(0, 20, 120, 20, Color.Purple, true);
+            graphics.DrawRectangle(0, 40, 120, 20, Color.Blue, true);
+            graphics.DrawRectangle(0, 60, 120, 20, Color.Green, true);
+            graphics.DrawRectangle(0, 80, 120, 20, Color.Yellow, true);
+            graphics.DrawRectangle(0, 100, 120, 20, Color.Orange, true);
+
+            graphics.Show();
+        }
+    }
+}
