2a341053c1cb1f0d16b10b1fcfcfc2387183e560
Adrian Stevens <adrian.stevens@gmail.com>
2022-01-10 11:39:32 -0800

    Add XML comments to Meadow.Units

--- source/Meadow.Units/AngularAcceleration.cs
+++ source/Meadow.Units/AngularAcceleration.cs
@@ -110,38 +110,176 @@ namespace Meadow.Units
         /// <returns></returns>
         [Pure] public AngularAcceleration Abs() { return new AngularAcceleration(Math.Abs(this.Value)); }
 
-        // ToString()
+        /// <summary>
+        /// Get a string represention of the object
+        /// </summary>
+        /// <returns>A string representing the object</returns>
         [Pure] public override string ToString() => Value.ToString();
+
+        /// <summary>
+        /// Get a string represention of the object
+        /// </summary>
+        /// <param name="format">format</param>
+        /// <param name="formatProvider">format provider</param>
+        /// <returns>A string representing the object</returns>
         [Pure] public string ToString(string format, IFormatProvider formatProvider) => Value.ToString(format, formatProvider);
 
         // IComparable
+        /// <summary>
+        /// Compare to another AbsoluteHumidity object
+        /// </summary>
+        /// <param name="obj">The other AbsoluteHumity cast to object</param>
+        /// <returns>0 if equal</returns>
         [Pure] public int CompareTo(object obj) => Value.CompareTo(obj);
+
+        /// <summary>
+        /// Get type code of object
+        /// </summary>
+        /// <returns>The TypeCode</returns>
         [Pure] public TypeCode GetTypeCode() => Value.GetTypeCode();
+
+        /// <summary>
+        /// Covert to boolean
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>bool representation of the object</returns>
         [Pure] public bool ToBoolean(IFormatProvider provider) => ((IConvertible)Value).ToBoolean(provider);
+
+        /// <summary>
+        /// Covert to byte
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>byte representation of the object</returns>
         [Pure] public byte ToByte(IFormatProvider provider) => ((IConvertible)Value).ToByte(provider);
+
+        /// <summary>
+        /// Covert to char
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>char representation of the object</returns>
         [Pure] public char ToChar(IFormatProvider provider) => ((IConvertible)Value).ToChar(provider);
+
+        /// <summary>
+        /// Covert to DateTime
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>DateTime representation of the object</returns>
         [Pure] public DateTime ToDateTime(IFormatProvider provider) => ((IConvertible)Value).ToDateTime(provider);
+
+        /// <summary>
+        /// Covert to Decimal
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>Decimal representation of the object</returns>
         [Pure] public decimal ToDecimal(IFormatProvider provider) => ((IConvertible)Value).ToDecimal(provider);
+
+        /// <summary>
+        /// Covert to double
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>double representation of the object</returns>
         [Pure] public double ToDouble(IFormatProvider provider) => Value;
+
+        /// <summary>
+        /// Covert to in16
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>int16 representation of the object</returns>
         [Pure] public short ToInt16(IFormatProvider provider) => ((IConvertible)Value).ToInt16(provider);
+
+        /// <summary>
+        /// Covert to int32
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>int32 representation of the object</returns>
         [Pure] public int ToInt32(IFormatProvider provider) => ((IConvertible)Value).ToInt32(provider);
+
+        /// <summary>
+        /// Covert to int64
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>int64 representation of the object</returns>
         [Pure] public long ToInt64(IFormatProvider provider) => ((IConvertible)Value).ToInt64(provider);
+
+        /// <summary>
+        /// Covert to sbyte
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>sbyte representation of the object</returns>
         [Pure] public sbyte ToSByte(IFormatProvider provider) => ((IConvertible)Value).ToSByte(provider);
+
+        /// <summary>
+        /// Covert to float
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>float representation of the object</returns>
         [Pure] public float ToSingle(IFormatProvider provider) => ((IConvertible)Value).ToSingle(provider);
+
+        /// <summary>
+        /// Covert to string
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>string representation of the object</returns>
         [Pure] public string ToString(IFormatProvider provider) => Value.ToString(provider);
+
+        /// <summary>
+        /// Covert to type
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>type representation of the object</returns>
         [Pure] public object ToType(Type conversionType, IFormatProvider provider) => ((IConvertible)Value).ToType(conversionType, provider);
+
+        /// <summary>
+        /// Covert to uint16
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>uint16 representation of the object</returns>
         [Pure] public ushort ToUInt16(IFormatProvider provider) => ((IConvertible)Value).ToUInt16(provider);
+
+        /// <summary>
+        /// Covert to uint32
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>uint32 representation of the object</returns>
         [Pure] public uint ToUInt32(IFormatProvider provider) => ((IConvertible)Value).ToUInt32(provider);
+
+        /// <summary>
+        /// Covert to uint64
+        /// </summary>
+        /// <param name="provider">format provider</param>
+        /// <returns>uint64 representation of the object</returns>
         [Pure] public ulong ToUInt64(IFormatProvider provider) => ((IConvertible)Value).ToUInt64(provider);
 
-        [Pure]
-        public int CompareTo(double? other)
+
+        /// <summary>
+        /// Compare the default value to a double 
+        /// </summary>
+        /// <param name="other">value to compare</param>
+        /// <returns>0 if equal</returns>
+        [Pure] public int CompareTo(double? other)
         {
             return (other is null) ? -1 : (Value).CompareTo(other.Value);
         }
 
+        /// <summary>
+        /// Compare the default value to a double 
+        /// </summary>
+        /// <param name="other">value to compare</param>
+        /// <returns>0 if equal</returns>
         [Pure] public bool Equals(double? other) => Value.Equals(other);
+
+        /// <summary>
+        /// Compare the default value to a double 
+        /// </summary>
+        /// <param name="other">value to compare</param>
+        /// <returns>0 if equal</returns>
         [Pure] public bool Equals(double other) => Value.Equals(other);
+
+        /// <summary>
+        /// Compare the default value to a double 
+        /// </summary>
+        /// <param name="other">value to compare</param>
+        /// <returns>0 if equal</returns>
         [Pure] public int CompareTo(double other) => Value.CompareTo(other);
     }
 }
