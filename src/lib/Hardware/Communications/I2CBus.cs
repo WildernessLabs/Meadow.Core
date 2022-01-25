ab715ee7c2108f5924e354cf1e35243d59c28396
ctacke <ctacke@opennetcf.com>
2022-01-25 11:06:20 -0600

    bug fix for i2c busses

--- src/lib/Hardware/Communications/I2CBus.cs
+++ src/lib/Hardware/Communications/I2CBus.cs
@@ -36,8 +36,7 @@ namespace Meadow
         public int BusNumber { get; set; } = 1;
         public Frequency Frequency { get; set; }
 
-        // the I2C block driver is...interesting.  You open it multiple times, once per peripheral address per bus
-        private Dictionary<byte, I2CPeripheralInfo> InfoMap0 { get; }
+        private PeripheralMap InfoMap { get; }
 
         public I2CBus(int busNumber, Frequency frequency)
         {
@@ -45,71 +44,17 @@ namespace Meadow
 
             // TODO: how do we affect frequency on these platforms?
             Frequency = frequency;
-
-            InfoMap0 = new(); // this is just for bus 0
+            InfoMap = new PeripheralMap(BusNumber);
         }
 
         public void Dispose()
         {
-            Interop.close(InfoMap0.First().Value.DriverHandle);
-        }
-
-        private int GetPeripheralHandle(int busNumber, byte busAddress)
-        {
-            Dictionary<byte, I2CPeripheralInfo> map;
-
-            switch(busNumber)
-            {
-                // TODO: we may need two info maps for platforms with multiple busses (e.g. jetson)
-                case 0:
-                case 1:
-                    map = InfoMap0;
-                    break;
-                default:
-                    throw new Exception($"Unsupported bus number: {busNumber}");
-            }
-
-            I2CPeripheralInfo info;
-
-            if (map.ContainsKey(busAddress))
-            {
-                info = map[busAddress];
-            }
-            else
-            {
-                // open the i2c block driver
-                var driver = $"/dev/i2c-{busNumber}";
-                var handle = Interop.open(driver, Interop.DriverFlags.O_RDWR);
-                if (handle < 0)
-                {
-                    // TODO: maybe try modprobe here?
-                    throw new Exception($"Unable to open driver {driver}.  Is it enabled on your platform?");
-                }
-                info = new I2CPeripheralInfo
-                {
-                    DriverHandle = handle,
-                    BusAddress = busAddress,
-                    BusNumber = busNumber
-                };
-                map.Add(busAddress, info);
-            }
-
-            if(!info.IsOpen)
-            {
-                // call the ioctl to set the address for this handle
-                var result = Interop.ioctl(info.DriverHandle, (int)I2CIoctl.SLAVE, info.BusAddress);
-                if(result < 0)
-                {
-                    Console.WriteLine($"ERROR: {Marshal.GetLastWin32Error()}");
-                }
-            }
-
-            return info.DriverHandle;
+            InfoMap.Dispose();
         }
 
         public unsafe void Read(byte peripheralAddress, Span<byte> readBuffer)
         {
-            var handle = GetPeripheralHandle(BusNumber, peripheralAddress);
+            var handle = InfoMap.GetAddressHandle(BusNumber, peripheralAddress);
             fixed (byte* pData = readBuffer)
             {
                 var result = Interop.read(handle, pData, readBuffer.Length);
@@ -130,7 +75,7 @@ namespace Meadow
 
         public unsafe void Write(byte peripheralAddress, Span<byte> writeBuffer)
         {
-            var handle = GetPeripheralHandle(BusNumber, peripheralAddress);
+            var handle = InfoMap.GetAddressHandle(BusNumber, peripheralAddress);
             fixed (byte* pData = writeBuffer)
             {
                 var result = Interop.write(handle, pData, writeBuffer.Length);
@@ -151,7 +96,7 @@ namespace Meadow
 
         public unsafe void Exchange(byte peripheralAddress, Span<byte> writeBuffer, Span<byte> readBuffer)
         {
-            var handle = GetPeripheralHandle(BusNumber, peripheralAddress);
+            var handle = InfoMap.GetAddressHandle(BusNumber, peripheralAddress);
             fixed (byte* pWrite = writeBuffer)
             fixed (byte* pRead = readBuffer)
             {
