d41c78c02c3fbd860e9e0bf92a39d9396a8b8d82
ctacke <ctacke@gmail.com>
2022-06-05 10:59:02 -0500

    Adding snickerdoodle black

---
+++ src/lib/Hardware/Pinouts/SnickerdoodleBlack.cs
@@ -0,0 +1,14 @@
+﻿using Meadow.Hardware;
+using System.Collections.Generic;
+
+namespace Meadow.Pinouts
+{
+    public class SnickerdoodleBlack : IPinDefinitions
+    {
+        public IList<IPin> AllPins => new List<IPin> 
+        {
+        };
+
+        public IPin GPIO1 => new GpiodPin("GPIO1", "PIN001", "gpiochip0", 1);
+    }
+}
