2a975b5539163b5c1e2b5de45163e9f7b4ad9316
ctacke <ctacke@gmail.com>
2022-08-23 09:58:15 -0500

    more CCM network bits

--- source/Meadow.F7/Devices/CcmWiredNetworkAdapter.cs
+++ source/Meadow.F7/Devices/CcmWiredNetworkAdapter.cs
@@ -3,50 +3,107 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 namespace Meadow.Devices
 {
     public class CcmWiredNetworkAdapter : IWiredNetworkAdapter
     {
+        public event NetworkConnectionHandler NetworkConnected;
+        public event NetworkDisconnectionHandler NetworkDisconnected;
+
+        private NetworkInterface? nativeInterface;
+
+        public bool IsConnected => false;
+        public PhysicalAddress MacAddress { get; private set; }
+
         internal CcmWiredNetworkAdapter()
         {
-            Resolver.Log.Info($"Creating network interface...");
+            LoadAdapterInfo();
+        }
 
-            try
+        public IPAddress IpAddress
+        {
+            get
             {
-                var interfaces = NetworkInterface.GetAllNetworkInterfaces();
-
-                Resolver.Log.Info($"{interfaces.Length} interfaces...");
-
-                foreach(var intf in interfaces)
+                if(nativeInterface == null)
                 {
-                    Resolver.Log.Info($"Interface: {intf.Id}: {intf.Name} {intf.NetworkInterfaceType} {intf.OperationalStatus}");
-
-                    var p = intf.GetIPProperties();
-                    Resolver.Log.Info($"MA: {p.MulticastAddresses.FirstOrDefault()}");
-                    Resolver.Log.Info($"GA: {p.GatewayAddresses.FirstOrDefault()}");
-
-                    var mac = intf.GetPhysicalAddress();
-                    Resolver.Log.Info($"Mac: {p.GatewayAddresses}");
+                    return IPAddress.None;
                 }
-            }
-            catch(Exception ex)
-            {
-                Resolver.Log.Error(ex.Message);
+
+                return nativeInterface.GetIPProperties()?.UnicastAddresses?.FirstOrDefault()?.Address ?? IPAddress.None;
             }
         }
 
-        public bool IsConnected => false;
+        public IPAddress SubnetMask
+        {
+            get
+            {
+                if(nativeInterface == null)
+                {
+                    return IPAddress.None;
+                }
 
-        public IPAddress IpAddress => IPAddress.None;
+                return nativeInterface.GetIPProperties()?.UnicastAddresses?.FirstOrDefault()?.IPv4Mask ?? IPAddress.None;
+            }
+        }
 
-        public IPAddress SubnetMask => IPAddress.None;
+        public IPAddress Gateway
+        {
+            get
+            {
+                if(nativeInterface == null)
+                {
+                    return IPAddress.None;
+                }
 
-        public IPAddress Gateway => IPAddress.None;
+                return nativeInterface.GetIPProperties()?.GatewayAddresses?.FirstOrDefault()?.Address ?? IPAddress.None;
+            }
+        }
 
-        public PhysicalAddress MacAddress => PhysicalAddress.None;
+        private void LoadAdapterInfo()
+        {
+            Task.Run(async () =>
+            {
+                Resolver.Log.Info($"Searching for wired network interface...");
 
-        public event NetworkConnectionHandler NetworkConnected;
-        public event NetworkDisconnectionHandler NetworkDisconnected;
+                try
+                {
+                    while(true)
+                    {
+                        var interfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+                        Resolver.Log.Info($"{interfaces.Length} interfaces...");
+
+                        if(interfaces.Length > 0)
+                        {
+                            foreach(var intf in interfaces)
+                            {
+                                Resolver.Log.Info($"Interface: {intf.Id}: {intf.Name} {intf.NetworkInterfaceType} {intf.OperationalStatus}");
+
+                                var p = intf.GetIPProperties();
+                                Resolver.Log.Info($"MA: {p.MulticastAddresses.FirstOrDefault()}");
+                                Resolver.Log.Info($"GA: {p.GatewayAddresses.FirstOrDefault()}");
+
+                                MacAddress = intf.GetPhysicalAddress();
+                                Resolver.Log.Info($"Mac: {MacAddress}");
+
+                                // TODO: check for wired (v. wifi)
+                                nativeInterface = intf;
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            await Task.Delay(TimeSpan.FromSeconds(5));
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Resolver.Log.Error(ex.Message);
+                }
+            });
+        }
     }
 }
