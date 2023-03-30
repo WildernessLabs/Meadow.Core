b7b03af6b36bf9c5428a1816fa5f5986b34a949a
Alexis Christoforides <alexis@thenull.net>
2023-03-30 18:57:25 -0300

    Fix marshalling for AES & add arg validation

--- source/implementations/f7/Meadow.F7/Interop/Interop.meadow_cloud_decrypt_buf_aes.cs
+++ source/implementations/f7/Meadow.F7/Interop/Interop.meadow_cloud_decrypt_buf_aes.cs
@@ -10,8 +10,9 @@ internal static partial class Interop
         [DllImport(LIBRARY_NAME, SetLastError = true)]
         private static extern int meadow_cloud_decrypt_buf_aes(IntPtr encrypted_buf, int encrypted_len, IntPtr key, IntPtr iv, IntPtr decrypted_buf);
 
-        const int aes_block_size = 16;
-        const int aes_key_size = 16;
+        const int aes_key_size = 32;
+
+        const int aes_iv_size = 16;
 
         public static byte[] MeadowCloudDecryptAES(byte[] encrypted_data, byte[] key, byte[] iv)
         {
@@ -20,11 +21,20 @@ internal static partial class Interop
             IntPtr keyBufferPtr = IntPtr.Zero;
             IntPtr ivBufferPtr = IntPtr.Zero;
 
+            if (encrypted_data.Length % 16 != 0)
+                 throw new ArgumentException(nameof(encrypted_data));
+
+            if (key.Length != aes_key_size)
+                throw new ArgumentException(nameof(key));
+
+            if (iv.Length != aes_iv_size)
+                throw new ArgumentException(nameof(iv));
+
             try
             {
                 encryptedBufferPtr = Marshal.AllocHGlobal(encrypted_data.Length);
                 decryptedBufferPtr = Marshal.AllocHGlobal(encrypted_data.Length);
-                ivBufferPtr = Marshal.AllocHGlobal(aes_block_size);
+                ivBufferPtr = Marshal.AllocHGlobal(aes_iv_size);
                 keyBufferPtr = Marshal.AllocHGlobal(aes_key_size);
 
                 Marshal.Copy(encrypted_data, 0, encryptedBufferPtr, encrypted_data.Length);
