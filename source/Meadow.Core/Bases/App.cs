7f26b69ae8e05d870f06968772814d12dcc354e0
Chris Tacke <ctacke@opennetcf.com>
2023-04-26 12:14:33 -0500

    provide a way for applications to pass an IApp instance to MeadowOS Start instead of requiring that we create it

--- source/Meadow.Core/Bases/App.cs
+++ source/Meadow.Core/Bases/App.cs
@@ -16,6 +16,9 @@
 
         public CancellationToken CancellationToken { get; internal set; }
 
+        /// <summary>
+        /// ase constructor for the App class
+        /// </summary>
         protected App()
         {
             executionContext = Thread.CurrentThread.ExecutionContext;
@@ -26,20 +29,41 @@
             Resolver.Services.Add<IApp>(this);
         }
 
-        public void InvokeOnMainThread(Action<object> action, object? state = null)
+        /// <summary>
+        /// Invokes an action in the context of the applications main thread
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="state"></param>
+        public void InvokeOnMainThread(Action<object?> action, object? state = null)
         {
             ExecutionContext.Run(executionContext, new ContextCallback(action), state);
         }
 
+        /// <summary>
+        /// Called by MeadowOS when everything is ready for the App to run
+        /// </summary>
+        /// <returns></returns>
         public virtual Task Run()
         {
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// Called by MeadowOS to initialize the App
+        /// </summary>
         public virtual Task Initialize() { return Task.CompletedTask; }
 
+        /// <summary>
+        /// Called when a request to shut down the App occurs
+        /// </summary>
+        /// <remarks>This is called by the Update Service before applying an update</remarks>
         public virtual Task OnShutdown() { return Task.CompletedTask; }
 
+        /// <summary>
+        /// Called when the MeadowOS encounters an error
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
         public virtual Task OnError(Exception e) { return Task.CompletedTask; }
 
         /// <summary>
@@ -61,13 +85,17 @@
         /// <summary>
         /// The root Device interface
         /// </summary>
-        public static D Device { get; protected set; }
+        public static D Device { get; protected set; } = default!;
 
         /// <summary>
         /// The app cancellation token
         /// </summary>
         public static CancellationToken Abort { get; protected set; }
 
+        /// <summary>
+        /// Virtual method provided for App implemenmtations to clean up resources on Disposal
+        /// </summary>
+        /// <returns></returns>
         public virtual ValueTask DisposeAsync() { return new ValueTask(Task.CompletedTask); }
     }
 }
