c56cabdc261e72ce0ef97f0d7a1222158a697576
Chris Tacke <ctacke@opennetcf.com>
2023-05-27 09:37:40 -0600

    added implementation for linux processor temp, moved proc temp in F7 to only the PlatformOS

--- source/implementations/linux/Meadow.Linux/PlatformOS/LinuxPlatformOS.cs
+++ source/implementations/linux/Meadow.Linux/PlatformOS/LinuxPlatformOS.cs
@@ -122,7 +122,18 @@ public class LinuxPlatformOS : IPlatformOS
 
     public virtual Temperature GetCpuTemperature()
     {
-        throw new PlatformNotSupportedException();
+        var fi = new FileInfo("/sys/class/thermal/thermal_zone0/temp");
+        if (!fi.Exists)
+        {
+            throw new PlatformNotSupportedException("CPU temp not available on this OS");
+        }
+
+        using (var r = fi.OpenText())
+        {
+            var data = r.ReadToEnd().Trim();
+            var temp = int.Parse(data);
+            return new Temperature(temp / 1000d, Temperature.UnitType.Celsius);
+        }
     }
 
     public bool RebootOnUnhandledException => false;
