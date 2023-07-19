0efbe7bb8a3f3bf6641f20a46f5a9e5c23523f26
Eduardo Menezes <eduardomenezesm@msn.com>
2023-07-19 13:49:19 -0300

    Enhance hardcoded variable on cell network adapter

--- source/Meadow.Core/CellNetworkAdapter.cs
+++ source/Meadow.Core/CellNetworkAdapter.cs
@@ -1,6 +1,7 @@
 ﻿using Meadow.Hardware;
 using System;
 using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 
 namespace Meadow.Devices
 {
@@ -8,8 +9,13 @@ namespace Meadow.Devices
     /// <summary>
     /// Represents a Cell network adapter
     /// </summary>
-    public class CellNetworkAdapter : NetworkAdapterBase, IWiredNetworkAdapter
+    public class CellNetworkAdapter : NetworkAdapterBase, ICellNetworkAdapter
     {
+        public const string LIBRARY_NAME = "nuttx";
+
+        [DllImport(LIBRARY_NAME, SetLastError = true)]
+        private static extern bool meadow_cell_is_connected();
+
         /// <summary>
         /// Creates an instance of a CellNetworkAdapter
         /// </summary>
@@ -31,10 +37,9 @@ namespace Meadow.Devices
         {
         }
 
-        // TODO: determine a way to detect this
         /// <summary>
         /// Returns <b>true</b> if the adapter is connected, otherwise <b>false</b>
         /// </summary>
-        public override bool IsConnected => true;
+        public override bool IsConnected => meadow_cell_is_connected();
     }
 }
