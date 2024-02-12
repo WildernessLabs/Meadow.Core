cbc0f4942ec2f3f3bd73dbbb22fc2402464daacf
Chris Tacke <ctacke@opennetcf.com>
2024-02-12 17:52:19 -0600

    desktop clean-up and doc

--- source/implementations/desktop/Meadow.Desktop/Desktop.cs
+++ source/implementations/desktop/Meadow.Desktop/Desktop.cs
@@ -9,6 +9,9 @@ public class Desktop : IMeadowDevice
 {
     private IMeadowDevice _implementation;
 
+    public event NetworkConnectionHandler NetworkConnected;
+    public event NetworkDisconnectionHandler NetworkDisconnected;
+
     public Desktop()
     {
     }
@@ -32,125 +35,57 @@ public class Desktop : IMeadowDevice
         {
             Display = displayProvider.CreateDisplay();
         }
+
+        _implementation.NetworkConnected += (s, e) => NetworkConnected?.Invoke(s, e);
+        _implementation.NetworkDisconnected += (s) => NetworkDisconnected?.Invoke(s);
     }
+
+    public virtual IPixelDisplay? Display { get; private set; }
 
     public IPlatformOS PlatformOS => _implementation.PlatformOS;
     public IDeviceInformation Information => _implementation.Information;
     public DeviceCapabilities Capabilities => _implementation.Capabilities;
     public INetworkAdapterCollection NetworkAdapters => _implementation.NetworkAdapters;
-
-    public IPixelDisplay Display { get; private set; }
-
-    /////////////////////////////////////////////////
-    /////////////////////////////////////////////////
-    /////////////////////////////////////////////////
-    public event NetworkConnectionHandler NetworkConnected;
-    public event NetworkDisconnectionHandler NetworkDisconnected;
-
-    public IPin GetPin(string name)
-    {
-        throw new NotImplementedException();
-    }
-
-    public BatteryInfo? GetBatteryInfo()
-    {
-        throw new NotImplementedException();
-    }
-
+    public IPin GetPin(string name) => _implementation.GetPin(name);
+    public BatteryInfo? GetBatteryInfo() => _implementation.GetBatteryInfo();
     public IDigitalInputPort CreateDigitalInputPort(IPin pin, ResistorMode resistorMode)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateDigitalInputPort(pin, resistorMode);
     public IDigitalInterruptPort CreateDigitalInterruptPort(IPin pin, InterruptMode interruptMode, ResistorMode resistorMode, TimeSpan debounceDuration, TimeSpan glitchDuration)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateDigitalInterruptPort(pin, interruptMode, resistorMode, debounceDuration, glitchDuration);
     public IBiDirectionalInterruptPort CreateBiDirectionalInterruptPort(IPin pin, bool initialState, InterruptMode interruptMode, ResistorMode resistorMode, PortDirectionType initialDirection, TimeSpan debounceDuration, TimeSpan glitchDuration, OutputType output = OutputType.PushPull)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateBiDirectionalInterruptPort(pin, initialState, interruptMode, resistorMode, initialDirection, debounceDuration, glitchDuration, output);
     public IBiDirectionalPort CreateBiDirectionalPort(IPin pin, bool initialState)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateBiDirectionalPort(pin, initialState);
     public IAnalogInputPort CreateAnalogInputPort(IPin pin, int sampleCount, TimeSpan sampleInterval, Voltage voltageReference)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateAnalogInputPort(pin, sampleCount, sampleInterval, voltageReference);
     public IAnalogInputArray CreateAnalogInputArray(params IPin[] pins)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateAnalogInputArray(pins);
     public IPwmPort CreatePwmPort(IPin pin, Frequency frequency, float dutyCycle = 0.5F, bool invert = false)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreatePwmPort(pin, frequency, dutyCycle, invert);
     public ISerialPort CreateSerialPort(SerialPortName portName, int baudRate = 9600, int dataBits = 8, Parity parity = Parity.None, StopBits stopBits = StopBits.One, int readBufferSize = 1024)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateSerialPort(portName, baudRate, dataBits, parity, stopBits, readBufferSize);
     public ISerialMessagePort CreateSerialMessagePort(SerialPortName portName, byte[] suffixDelimiter, bool preserveDelimiter, int baudRate = 9600, int dataBits = 8, Parity parity = Parity.None, StopBits stopBits = StopBits.One, int readBufferSize = 512)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateSerialMessagePort(portName, suffixDelimiter, preserveDelimiter, baudRate, dataBits, parity, stopBits, readBufferSize);
     public ISerialMessagePort CreateSerialMessagePort(SerialPortName portName, byte[] prefixDelimiter, bool preserveDelimiter, int messageLength, int baudRate = 9600, int dataBits = 8, Parity parity = Parity.None, StopBits stopBits = StopBits.One, int readBufferSize = 512)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateSerialMessagePort(portName, prefixDelimiter, preserveDelimiter, messageLength, baudRate, dataBits, parity, stopBits, readBufferSize);
     public ISpiBus CreateSpiBus(IPin clock, IPin copi, IPin cipo, SpiClockConfiguration config)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateSpiBus(clock, copi, cipo, config);
     public ISpiBus CreateSpiBus(IPin clock, IPin copi, IPin cipo, Frequency speed)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateSpiBus(clock, copi, cipo, speed);
     public IDigitalOutputPort CreateDigitalOutputPort(IPin pin, bool initialState = false, OutputType initialOutputType = OutputType.PushPull)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateDigitalOutputPort(pin, initialState, initialOutputType);
     public II2cBus CreateI2cBus(int busNumber = 1, I2cBusSpeed busSpeed = I2cBusSpeed.Standard)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateI2cBus(busNumber, busSpeed);
     public II2cBus CreateI2cBus(IPin[] pins, I2cBusSpeed busSpeed)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateI2cBus(pins, busSpeed);
     public II2cBus CreateI2cBus(IPin clock, IPin data, I2cBusSpeed busSpeed)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.CreateI2cBus(clock, data, busSpeed);
     public void WatchdogEnable(TimeSpan timeout)
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.WatchdogEnable(timeout);
     public void WatchdogReset()
-    {
-        throw new NotImplementedException();
-    }
-
+        => _implementation.WatchdogReset();
     public ICounter CreateCounter(IPin pin, InterruptMode edge)
-    {
-        throw new NotImplementedException();
-    }
+        => _implementation.CreateCounter(pin, edge);
 }
 
 
---
+++ source/implementations/desktop/Meadow.Desktop/readme.md
@@ -0,0 +1,103 @@
+﻿# `Meadow.Desktop`
+
+`Meadow.Desktop` is a cross-platform abstraction for using the Meadow software stack on Desktop operating systems (Windows, Mac and Linux).
+
+## Buses and Peripherals
+
+Since most desktop machines do not have GPIO or typical embedded communication buses, in order to communicate with SPI or I2C devices you must use an IO Expander such as the FT232H or CP2112.
+
+For example, creating an I2C periperhal using an expander might look like this:
+
+```
+var expanders = new FT232Collection();
+var i2cBus = expanders[0].CreateI2cBus();
+var peripheral = new Bme280(i2cBus)
+```
+
+## Prototyping embedded user itnerfaces
+
+If you are prototyping a user interface that uses a Meadow.Foundation `IPixelDisplay`, then `Meadow.Desktop` provides a cross-platform display simulation.  The simulated display is available through the `Device.Display` property.
+
+For example, if you are using MicroLayout as your layout engine, you can initialize a screen with the following:
+
+```
+var screen = new DisplayScreen(Device.Display);
+```
+
+In all cases, the display requires an underlying rendering loop to be run to keep the display updated, and your application must call that rendering loop.  The simplest way to do this is:
+
+```
+public override Task Run()
+{
+    // NOTE: this will not return until the display is closed
+    ExecutePlatformDisplayRunner();
+
+    return Task.CompletedTask;
+}
+
+```
+
+### Windows
+
+On Windows machines, the `IPixelDisplay` uses WinForms under the hood.  This requires a few housekeeping items.
+
+First, you must make adjustment to the application project file.  You muust add `net8.0-windows` to your `TargetFrameworks` and you must add `UseWindowsForms` to that target framework.  Below are the relevent project file sections.
+
+```
+<Project Sdk="Microsoft.NET.Sdk">
+    <PropertyGroup>
+        <OutputType>Exe</OutputType>
+        <TargetFrameworks>net8.0;net8.0-windows</TargetFrameworks>
+    </PropertyGroup>
+
+    <PropertyGroup Condition=" '$(TargetFramework)' == 'net8.0-windows'">
+        <UseWindowsForms>true</UseWindowsForms>
+    </PropertyGroup>
+</Project>
+```
+
+Second, WinForms needs some initialization.  Do this in the application's `Main` entry point.
+
+```
+public static async Task Main(string[] args)
+{
+#if WINDOWS
+    System.Windows.Forms.Application.EnableVisualStyles();
+    System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+    ApplicationConfiguration.Initialize();
+#endif
+    await MeadowOS.Start(args);
+}
+
+```
+
+
+### Mac
+
+`Meadow.Desktop`'s display on Macintosh relies on GTK, so GTK must be installed on the machine.
+
+#### Install Brew
+
+If Brew is not already installed, install it:
+
+```
+$ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+#### Install GTK
+
+To install gtk+3 on macOS using brew, run the following command:
+
+```
+$ brew install gtk+3
+$ brew install gtksourceview4
+```
+
+### Linux
+
+To install gtk+3 on Linux, run the following command:
+
+```
+$ sudo apt-get install libgtk-3-dev
+```
+
