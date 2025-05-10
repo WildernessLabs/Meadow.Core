076f703ff19db6ed9d63c8faa2a784767c2e972d
Adrian Stevens <adrian.stevens@gmail.com>
2025-05-10 11:13:45 -0700

    XML + metadata cleanup

--- Source/ui/Meadow.Avalonia/AvaloniaMeadowApplication.cs
+++ Source/ui/Meadow.Avalonia/AvaloniaMeadowApplication.cs
@@ -1,25 +1,29 @@
 ﻿using Avalonia;
 using Avalonia.Threading;
 
-namespace Meadow.UI
+namespace Meadow.Avalonia
 {
     /// <summary>
-    /// A base class for Avalonia UI Applications using the Meadow stack
+    /// A base class for Avalonia UI Applications using the Meadow stack.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The type of the Meadow device that implements <see cref="IMeadowDevice"/>.</typeparam>
     public class AvaloniaMeadowApplication<T> : Application, IApp
         where T : class, IMeadowDevice
     {
+        /// <inheritdoc/>
         public CancellationToken CancellationToken => throw new NotImplementedException();
 
         /// <summary>
-        /// The IMeadowDevice
+        /// Gets the Meadow device instance.
         /// </summary>
         public static T Device => Resolver.Services.Get<IMeadowDevice>() as T;
 
         /// <inheritdoc/>
         public Dictionary<string, string> Settings { get; internal set; } = new();
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AvaloniaMeadowApplication{T}"/> class.
+        /// </summary>
         protected AvaloniaMeadowApplication()
         {
         }
@@ -28,10 +32,10 @@ namespace Meadow.UI
         public virtual void OnBootFromCrash(IEnumerable<string> crashReports) { }
 
         /// <summary>
-        /// Invokes an action on the main (UI) thread
+        /// Invokes an action on the main (UI) thread.
         /// </summary>
-        /// <param name="action">The action to invoke</param>
-        /// <param name="state">Optional state information to pass as a parameter to the Action</param>
+        /// <param name="action">The action to invoke.</param>
+        /// <param name="state">Optional state information to pass as a parameter to the action.</param>
         public void InvokeOnMainThread(Action<object?> action, object? state = null)
         {
             Dispatcher.UIThread.Post(() => action(state));
@@ -73,16 +77,21 @@ namespace Meadow.UI
             return Task.CompletedTask;
         }
 
+        /// <inheritdoc/>
         Task IApp.Run()
         {
             return MeadowRun();
         }
 
+        /// <inheritdoc/>
         Task IApp.Initialize()
         {
             return MeadowInitialize();
         }
 
+        /// <summary>
+        /// Loads MeadowOS in a background thread.
+        /// </summary>
         protected void LoadMeadowOS()
         {
             new Thread((o) =>
