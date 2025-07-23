f2be1cca93a6b7dc10037bbb193596e1df8ae094
Chris Tacke <ctacke@opennetcf.com>
2025-07-22 21:29:41 -0500

    add Windows NTP client support

---
+++ Source/implementations/windows/Meadow.Windows/WindowsNtpClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Meadow;
+
+public class WindowsNtpClient : NtpClientBase
+{
+    /// <summary>
+    /// Returns <c>true</c> if the NTP Client is enabled
+    /// </summary>
+    public override bool Enabled => true;
+
+    internal WindowsNtpClient()
+    { }
+
+    /// <summary>
+    /// Time period that the NTP client attempts to query the NTP time server(s)
+    /// </summary>
+    public override TimeSpan PollPeriod
+    {
+        get => TimeSpan.Zero; // currently only happens at startup
+        set => throw new PlatformNotSupportedException("Changing NTP Poll Frequency not currently supported");
+    }
+
+    internal void RaiseTimeChanged()
+    {
+        RaiseTimeChanged(DateTime.UtcNow);
+    }
+}
